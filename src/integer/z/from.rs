e0af899ae63ea386a5cd7ef0a2540d0e3a816a91
Marcel Schmidt <marcel.schmidtfuldatal@gmail.com>
2023-03-01 14:09:46 +0100

    fixed format

--- src/integer/z/from.rs
+++ src/integer/z/from.rs
@@ -4,7 +4,7 @@
 //!
 //! The explicit functions contain the documentation.
 
-use flint_sys::fmpz::{fmpz, fmpz_init_set_si, fmpz_set_str, fmpz_init_set_ui};
+use flint_sys::fmpz::{fmpz, fmpz_init_set_si, fmpz_init_set_ui, fmpz_set_str};
 
 use super::Z;
 use crate::{error::MathError, macros};
