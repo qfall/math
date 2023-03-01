f11017fa254e3481a66351706b1064030eb6307e
Sven Moog <smoog@mail.upb.de>
2023-03-01 15:45:42 +0100

    Add `try_from_z` for `Modulus`

--- src/integer_mod_q/modulus/from.rs
+++ src/integer_mod_q/modulus/from.rs
@@ -11,9 +11,43 @@ use flint_sys::{
     fmpz_mod::{fmpz_mod_ctx, fmpz_mod_ctx_init},
 };
 
-use crate::error::MathError;
+use crate::{error::MathError, integer::Z};
 
 use super::Modulus;
+
+impl Modulus {
+    /// Create a [`Modulus`] from [`Z`].
+    ///
+    /// Parameters:
+    /// - `value`: the value of the modulus
+    ///
+    /// Returns a [`Modulus`] or an [`MathError`]
+    ///
+    /// # Example
+    /// ```rust
+    /// use math::integer_mod_q::Modulus;
+    /// use std::str::FromStr;
+    ///
+    /// let modulus = Modulus::from_str("42").unwrap();
+    /// ```
+    /// # Errors and Failures
+    ///
+    /// - Returns a [`MathError`] of type [`InvalidIntToModulus`](MathError::InvalidIntToModulus)
+    /// if the provided value is not greater than zero.
+    pub fn try_from_z(value: &Z) -> Result<Self, MathError> {
+        let modulus = ctx_init(&value.value);
+        Ok(Self { modulus: modulus? })
+    }
+}
+
+// TODO: write macro to generate [`TryFrom`] trait.
+impl TryFrom<&Z> for Modulus {
+    type Error = MathError;
+    /// Create [`Modulus`] from [`Z`] reference using [`try_from_z`](Modulus::try_from_z)
+    fn try_from(value: &Z) -> Result<Self, Self::Error> {
+        Modulus::try_from_z(value)
+    }
+}
 
 impl FromStr for Modulus {
     type Err = MathError;
@@ -85,6 +119,57 @@ fn ctx_init(n: &fmpz) -> Result<fmpz_mod_ctx, MathError> {
     unsafe {
         fmpz_mod_ctx_init(ctx.as_mut_ptr(), n);
         Ok(ctx.assume_init())
+    }
+}
+
+#[cfg(test)]
+mod test_try_from_z {
+    use super::Modulus;
+    use crate::integer::Z;
+
+    /// Demonstrate different ways to create a [`Modulus`] from a [`Z`] reference.
+    #[test]
+    fn z_to_modulus_reference() {
+        let value = Z::from(10);
+
+        let _: Modulus = (&value).try_into().unwrap();
+        let _: Modulus = <&Z>::try_into(&value).unwrap();
+        assert!(Modulus::try_from_z(&value).is_ok());
+        assert!(Modulus::try_from(&value).is_ok());
+    }
+
+    /// Test [`Modulus`] creation with small positive [`Z`] (valid).
+    #[test]
+    fn z_valid_small() {
+        let z = Z::from(10);
+
+        assert!(Modulus::try_from_z(&z).is_ok());
+    }
+
+    /// Test [`Modulus`] creation with large [`Z`] (valid).
+    /// (uses FLINT's pointer representation)
+    #[test]
+    fn z_valid_large() {
+        let z = Z::from(u64::MAX);
+
+        assert!(Modulus::try_from_z(&z).is_ok());
+    }
+
+    /// Test [`Modulus`] creation with small negative [`Z`] (invalid).
+    #[test]
+    fn z_negative_small() {
+        let z = Z::from(-10);
+
+        assert!(Modulus::try_from_z(&z).is_err());
+    }
+
+    /// Test [`Modulus`] creation with large negative [`Z`] (invalid).
+    /// (uses FLINT's pointer representation)
+    #[test]
+    fn z_negative_large() {
+        let z = Z::from(i64::MIN);
+
+        assert!(Modulus::try_from_z(&z).is_err());
     }
 }
 
