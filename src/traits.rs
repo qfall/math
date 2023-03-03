9b7be021679b5d26f9b59f931c15d7771547c4bb
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-03-03 16:46:06 +0100

    add doc comments

--- src/traits.rs
+++ src/traits.rs
@@ -1,3 +1,13 @@
+//! Traits implemented in this library. These include
+//! specific traits for matrices and polynomials.
+
+/// Is implemented by polynomials to evaluate it for a certain input.
 pub trait Evaluate<U, V> {
+    /// Evaluates the object for a given input value
+    ///
+    /// Parameters:
+    /// - `value`: The value with which to evaluate the object.
+    ///
+    /// Returns the evaluation of the object.
     fn evaluate<T: Into<U>>(&self, value: T) -> V;
 }
