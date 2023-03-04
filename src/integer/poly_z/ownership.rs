0c17b62c3c9e9f365d461cef74fb57e8879f0153
Marvin Beckmann <marvin17@mail.upb.de>
2023-03-04 12:01:45 +0100

    add tests for clone

--- src/integer/poly_z/ownership.rs
+++ src/integer/poly_z/ownership.rs
@@ -62,3 +62,56 @@ impl Drop for PolyZ {
         unsafe { fmpz_poly_clear(&mut self.poly) }
     }
 }
+
+/// Test that the [`Clone`] trait is correctly implemented.
+#[cfg(test)]
+mod test_clone {
+    use crate::integer::PolyZ;
+    use std::str::FromStr;
+
+    /// check if a clone of a PolyZ with an entry larger than 64 bits
+    /// works
+    #[test]
+    fn large_entries() {
+        let u64_string = u64::MAX.to_string();
+        let input = format!("2  {} -{}", u64_string, u64_string);
+
+        let poly_1 = PolyZ::from_str(&input).unwrap();
+        let poly_2 = poly_1.clone();
+
+        // tests where the first coefficient is stored. Since both are larger than
+        // an i64, both should be a pointer and their values should differ
+        unsafe {
+            assert_ne!((*poly_1.poly.coeffs).0, (*poly_2.poly.coeffs).0);
+        }
+        assert_eq!(poly_1.to_string(), poly_2.to_string())
+    }
+
+    /// check if several instantiations are cloned correctly
+    #[test]
+    fn small_examples() {
+        let pos_1 = PolyZ::from_str("2  0 11").unwrap();
+        let zero_1 = PolyZ::from_str("2  0 -11").unwrap();
+        let neg_1 = PolyZ::from_str("2  0 1100").unwrap();
+
+        let pos_2 = pos_1.clone();
+        let zero_2 = zero_1.clone();
+        let neg_2 = neg_1.clone();
+
+        assert_eq!(pos_1.to_string(), pos_2.to_string());
+        assert_eq!(zero_1.to_string(), zero_2.to_string());
+        assert_eq!(neg_1.to_string(), neg_2.to_string());
+    }
+
+    /// check if a cloned value is still alive after the original value ran out of scope
+    #[test]
+    #[allow(clippy::redundant_clone)]
+    fn keep_alive() {
+        let a: PolyZ;
+        {
+            let b = PolyZ::from_str("2  0 1").unwrap();
+            a = b.clone();
+        }
+        assert_eq!("2  0 1", a.to_string());
+    }
+}
