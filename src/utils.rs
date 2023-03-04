89cc61514544b7bbc418b2140c5e0d808e8cdd57
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-03-04 18:34:02 +0100

    fix naming error

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-//! This crate contains common functions that are used by several crates.
+//! This module contains common functions that are used by several crates.
 //!
 //! This can include functions to pre-process inputs, find size of matrices
 //! and similar tasks.
