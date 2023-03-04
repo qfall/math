77e34f21333edd215386842fee17c4b88b0a1a89
Marvin Beckmann <marvin17@mail.upb.de>
2023-03-04 11:37:53 +0100

    add drop trait for polyZ

--- src/integer/poly_z/ownership.rs
+++ src/integer/poly_z/ownership.rs
@@ -13,9 +13,8 @@
 //! drop(a);
 //! ```
 
-use flint_sys::fmpz_poly::fmpz_poly_set;
-
 use super::PolyZ;
+use flint_sys::fmpz_poly::{fmpz_poly_clear, fmpz_poly_set};
 
 impl Clone for PolyZ {
     /// Clones the given element and returns a deep clone of the [`PolyZ`] element.
@@ -34,5 +33,29 @@ impl Clone for PolyZ {
         unsafe { fmpz_poly_set(&mut value.poly, &self.poly) }
 
         value
+    }
+}
+
+impl Drop for PolyZ {
+    /// Drops the given [`PolyZ`] value and frees the allocated memory.
+    ///
+    /// # Examples
+    /// ```
+    /// use math::integer::PolyZ;
+    /// use std::str::FromStr;
+    /// {
+    ///     let a = PolyZ::from_str("3  0 1 2");
+    /// } // as a's scope ends here, it get's dropped
+    /// ```
+    ///
+    /// ```
+    /// use math::integer::PolyZ;
+    /// use std::str::FromStr;
+    ///
+    /// let a = PolyZ::from_str("3  0 1 2");
+    /// drop(a); // explicitly drops a's value
+    /// ```
+    fn drop(&mut self) {
+        unsafe { fmpz_poly_clear(&mut self.poly) }
     }
 }
