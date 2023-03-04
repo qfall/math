451353d9ab437e5282d34a58c0378db447e256c5
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-03-04 01:24:03 +0100

    add tests for evaluate coordinate

--- src/utils/coordinate.rs
+++ src/utils/coordinate.rs
@@ -12,6 +12,13 @@ use std::fmt::Display;
 /// Returns an [`i64`] representation of the coordinate or an error, if the
 /// coordinate does not fullfil all conditions.
 ///
+/// # Example
+/// ```rust
+/// use math::utils::coordinate::evaluate_coordinate;
+///
+/// let coordinate = evaluate_coordinate(u32::MAX).unwrap();
+/// ```
+///
 /// # Errors and Failures
 /// - Returns a [`MathError`] of type [`OutOfBounds`](MathError::OutOfBounds) if
 /// either the coordinate is negative or it does not fit into an [`i64`].
@@ -37,4 +44,34 @@ pub fn evaluate_coordinate<S: TryInto<i64> + Display + Copy>(
         ));
     }
     Ok(coordinate)
+}
+
+#[cfg(test)]
+mod test_eval_coordinate {
+    use super::evaluate_coordinate;
+
+    /// tests that negative coordinates are not accepted
+    #[test]
+    fn is_err_negative() {
+        assert!(evaluate_coordinate(i32::MIN).is_err())
+    }
+
+    /// tests that the function can be called with several types
+    #[test]
+    fn is_ok_several_types() {
+        assert!(evaluate_coordinate(i8::MAX).is_ok());
+        assert!(evaluate_coordinate(i16::MAX).is_ok());
+        assert!(evaluate_coordinate(i32::MAX).is_ok());
+        assert!(evaluate_coordinate(i64::MAX).is_ok());
+        assert!(evaluate_coordinate(u8::MAX).is_ok());
+        assert!(evaluate_coordinate(u16::MAX).is_ok());
+        assert!(evaluate_coordinate(u32::MAX).is_ok());
+    }
+
+    /// ensure that integers which can not be converted to an [`i64`]
+    /// are not accepted
+    #[test]
+    fn does_not_fit() {
+        assert!(evaluate_coordinate(u64::MAX).is_err());
+    }
 }
