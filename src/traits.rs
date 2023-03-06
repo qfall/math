00f9e41f57477f8086c41f83d80118d9a8921c19
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-03-06 14:52:54 +0100

    adapt comments in trait module

--- src/traits.rs
+++ src/traits.rs
@@ -1,9 +1,10 @@
-//! Traits implemented in this library. These include
+//! This module contains basic traits for this library. These include
 //! specific traits for matrices and polynomials.
 
 /// Is implemented by polynomials to evaluate it for a certain input.
 pub trait Evaluate<U, V> {
-    /// Evaluates the object for a given input value.
+    /// Evaluates the object, e.g. polynomial or a matrix of polynomials,
+    /// for a given input value.
     ///
     /// Parameters:
     /// - `value`: The value with which to evaluate the object.
