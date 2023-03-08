f029e3758f8268062cf72e8bbbdbceabf3185c1e
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-03-08 13:08:02 +0100

    Add doc comments to structs

--- src/integer/z.rs
+++ src/integer/z.rs
@@ -11,6 +11,28 @@ mod ownership;
 
 #[allow(dead_code)]
 #[derive(Debug)]
+/// [`Z`] represents any integer value.
+///
+/// Attributes:
+/// - `value`: holds [FLINT](https://flintlib.org/)'s [struct](fmpz)
+///     for an integer value
+///
+/// # Examples
+/// ```
+/// use math::integer::Z;
+/// use std::str::FromStr;
+///
+/// let a = Z::from_str("-876543")?;
+/// let b = Z::from_i64(i64::MIN);
+/// let zero = Z::default();
+///
+/// let result = &a - b.clone() + &zero;
+///
+/// drop(b);
+///
+/// assert_ne!(result, zero);
+/// # Ok::<(), math::error::MathError>(())
+/// ```
 pub struct Z {
     pub(crate) value: fmpz,
 }
