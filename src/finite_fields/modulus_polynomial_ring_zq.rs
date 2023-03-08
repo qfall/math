52edceb7d1bf9ed30d0c54f95fe6f6551697e7b5
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-03-08 23:58:13 +0100

    document structs

--- src/finite_fields/modulus_polynomial_ring_zq.rs
+++ src/finite_fields/modulus_polynomial_ring_zq.rs
@@ -8,6 +8,22 @@ mod from;
 mod get;
 mod to_string;
 
+/// [`ModulusPolynomialRingZq`] represents the modulus object for
+/// [`PolynomialRingZq`](crate::finite_fields::PolynomialRingZq)
+///
+/// Attributes
+/// - `modulus`: holds the specific content, i.e. the prime q and f(X); it
+/// holds [FLINT](https://flintlib.org/)'s [struct](fq_ctx_struct)
+///
+/// # Example
+/// ```rust
+/// use math::finite_fields::ModulusPolynomialRingZq;
+/// use math::integer_mod_q::PolyOverZq;
+/// use std::str::FromStr;
+///
+/// // initialize X^2 + 1 mod 17, i.e. an irreducible polynomial with prime modulus
+/// let poly_mod = PolyOverZq::from_str("3  1 0 1 mod 17").unwrap();
+/// let modulus = ModulusPolynomialRingZq::from(&poly_mod);
 pub struct ModulusPolynomialRingZq {
     modulus: fq_ctx_struct,
 }
--- src/finite_fields/polynomial_ring_zq.rs
+++ src/finite_fields/polynomial_ring_zq.rs
@@ -6,6 +6,14 @@ use super::ModulusPolynomialRingZq;
 use crate::integer::PolyOverZ;
 
 #[allow(dead_code)]
+/// [`PolynomialRingZq`] represents polynomials over the finite field
+/// Zq[X]/f(X) where q is prime and f(X) is irreducible.
+///
+/// Attributes
+/// - `poly`: holds the value
+/// - `modulus`: holds the prime q and f(X)
+///
+/// # Example
 pub struct PolynomialRingZq {
     poly: PolyOverZ,
     modulus: ModulusPolynomialRingZq,
