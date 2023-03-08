ec3868a08c20cd1a4224210f79316e8a8848c8e5
3pmile <pmile@mail.uni-paderborn.de>
2023-03-08 14:25:16 +0100

    waiting for comp

--- src/rational/q.rs
+++ src/rational/q.rs
@@ -3,6 +3,7 @@
 
 use flint_sys::fmpq::fmpq;
 
+mod arithmetic;
 mod default;
 mod from;
 
---
+++ src/rational/q/arithmetic.rs
@@ -0,0 +1,5 @@
+//! The arithmetic module contains implementations of simple arithmetics
+//! such as addition or subtraction.
+
+mod add;
+mod sub;
