984fff22b8dcb8e27bca6c575f1274f9248610b9
Marvin Beckmann <marvin17@mail.upb.de>
2023-03-09 11:19:53 +0100

    add tests for modulus is prime

--- src/integer_mod_q/modulus/properties.rs
+++ src/integer_mod_q/modulus/properties.rs
@@ -7,3 +7,24 @@ impl Modulus {
         1 == unsafe { fmpz_is_prime(&self.get_fq_ctx_struct().n[0]) }
     }
 }
+
+#[cfg(test)]
+mod test_is_prime {
+    use std::str::FromStr;
+
+    use crate::integer_mod_q::Modulus;
+
+    /// ensure that if a [`Modulus`] is instantiated with a prime, `true` is returned
+    #[test]
+    fn modulus_is_prime() {
+        let modulus = Modulus::from_str(&format!("{}", 2_i32.pow(16) + 1)).unwrap();
+        assert!(modulus.is_prime())
+    }
+
+    /// ensure that if a [`Modulus`] is instantiated with a non-prime, `false` is returned
+    #[test]
+    fn modulus_is_not_prime() {
+        let modulus = Modulus::from_str(&format!("{}", 2_i32.pow(16))).unwrap();
+        assert!(!modulus.is_prime())
+    }
+}
