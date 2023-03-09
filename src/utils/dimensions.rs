d34b8b8da3a0a5b929fc1e435a84dc0be1da60fe
Marcel Schmidt <marcel.schmidtfuldatal@gmail.com>
2023-03-09 13:48:39 +0100

    add suggestions

--- src/utils/dimensions.rs
+++ src/utils/dimensions.rs
@@ -3,29 +3,19 @@
 use crate::error::MathError;
 
 /// Returns the matrix dimensions of a prepared string
-/// Takes `1,2,3|4,5,6` as input and outputs `(2,3)` accordingly.
+/// Takes `1, 2, 3|4, 5, 6` as input and outputs `(2,3)` accordingly.
 ///
-/// Returns an error if the if the number of rows or columns is too big
+/// Returns an error if the number of rows or columns is too big
 /// (must fit into [`i64`]) or if the number of entries in rows is unequal.
 ///
 /// Input parameters:
 /// - `string`: the string of the matrix
 ///
-/// # Example
-/// ```rust
-/// use math::utils::dimensions::find_matrix_dimensions;
-/// use math::utils::prepare::prepare_matrix_string;
-///
-/// let string = String::from("[[1, 2, 3],[3, 4, 5]]");
-/// let prepared_string = prepare_matrix_string(&string).unwrap();
-/// let dimensions = find_matrix_dimensions(&string).unwrap();
-/// ```
-///
 /// # Errors and Failures
 /// - Returns a [`MathError`] of type [`InvalidMatrix`](MathError::InvalidMatrix)
 /// if the number of rows or columns is too big (must fit into [`i64`]) or
 /// if the number of entries in rows is unequal.
-pub fn find_matrix_dimensions(string: &str) -> Result<(i64, i64), MathError> {
+pub(crate) fn find_matrix_dimensions(string: &str) -> Result<(i64, i64), MathError> {
     let mut num_rows: i64 = match string.matches('|').count().try_into() {
         Ok(num_rows) => num_rows,
         _ => {
@@ -62,25 +52,30 @@ pub fn find_matrix_dimensions(string: &str) -> Result<(i64, i64), MathError> {
 
 #[cfg(test)]
 mod test_find_matrix_dimensions {
-    use crate::utils::dimensions::find_matrix_dimensions;
-    use crate::utils::prepare::prepare_matrix_string;
+    use crate::utils::{dimensions::find_matrix_dimensions, prepare::prepare_matrix_string};
 
     // Ensure that correct prepared strings of a matrix are accepted.
     #[test]
     fn correct_matrix_works() {
         let mut matrix_string1 = String::from("[[1, 2, 3],[3, 4, 5]]");
         let mut matrix_string2 = String::from("[[1/3, -2/7, 3],[3, 4, -5/-2]]");
+        let mut matrix_string3 = String::from("[[4  0 1 2 3, 2  0 1],[1  5, 2  7 8]]");
+        let mut matrix_string4 = String::from("[[sdclin, =§&%, +57n4],[+dk<, 37 ffew, 8fh2n]]");
 
         matrix_string1 = prepare_matrix_string(&matrix_string1).unwrap();
         matrix_string2 = prepare_matrix_string(&matrix_string2).unwrap();
+        matrix_string3 = prepare_matrix_string(&matrix_string3).unwrap();
+        matrix_string4 = prepare_matrix_string(&matrix_string4).unwrap();
 
         assert!(find_matrix_dimensions(&matrix_string1).is_ok());
         assert!(find_matrix_dimensions(&matrix_string2).is_ok());
+        assert!(find_matrix_dimensions(&matrix_string3).is_ok());
+        assert!(find_matrix_dimensions(&matrix_string4).is_ok());
     }
 
-    // Ensure that incorrect prepared strings of a matrix are rejected.
+    // Ensure that a matrix with an incorrect number of entries in rows is rejected.
     #[test]
-    fn incorrect_matrix_error() {
+    fn incorrect_rows_error() {
         let matrix_string1 = String::from("1,2|3,4,5");
 
         assert!(find_matrix_dimensions(&matrix_string1).is_err());
@@ -101,5 +96,8 @@ mod test_find_matrix_dimensions {
 
         assert!(find_matrix_dimensions(&s1).is_ok());
         assert!(find_matrix_dimensions(&s2).is_ok());
+
+        assert_eq!(find_matrix_dimensions(&s1).unwrap().1, 650001);
+        assert_eq!(find_matrix_dimensions(&s2).unwrap().0, 650001);
     }
 }
--- src/utils/prepare.rs
+++ src/utils/prepare.rs
@@ -5,60 +5,30 @@ use regex::Regex;
 
 /// Takes the string of a matrix as input and prepares it for easy use.
 ///
-/// The input should look like `[[1, 2, 3],[4, 5, 6]]` to get `1,2,3|4,5,6` as output.
-/// Symbols that are not `-`, `/`, `,`, `[`, `]` or digits between 0 and 9 are
-/// automatically deleted.
-///
-/// Entries of the matrix can contain the symbols `-`, `/` and digits between
-/// 0 and 9. They need to have at least one symbol.
-///
+/// The input should look like `[[1, 2, 3],[4, 5, 6]]` to get `1, 2, 3|4, 5, 6` as output.
+/// Entries of the matrix can contain all symbols but `[`, `]` and `,`.
 ///
 /// If the matrix is not formatted correctly, an error is returned.
 ///
 /// Input parameters:
 /// - `string`: a matrix as a string
 ///
-/// # Examples
-/// ```rust
-/// use math::utils::prepare::prepare_matrix_string;
-///
-/// let string = String::from("[[1, 2, 3],[3, 4, 5]]");
-/// let prepared_string = prepare_matrix_string(&string).unwrap();
-/// ```
-///
-/// ```rust
-/// use math::utils::prepare::prepare_matrix_string;
-///
-/// let string = String::from("[[1/3, -2/7, 3],[3, 4, -5/-2]]");
-/// let prepared_string = prepare_matrix_string(&string).unwrap();
-/// ```
-///
 /// # Errors and Failures
 /// - Returns a [`MathError`] of type [`InvalidMatrix`](MathError::InvalidMatrix)
 /// if the matrix is not formatted in a suitable way.
 /// - Returns a [`MathError`] of type [`RegexError`](MathError::RegexError)
 /// if the regular expression inside of the function could not be processed.
-pub fn prepare_matrix_string(string: &str) -> Result<String, MathError> {
-    // remove any white space, newline or other characters, that should not be included or aren't necessary
-    let string: String = string
-        .chars()
-        .filter(|c| {
-            c.is_ascii_digit() || *c == '[' || *c == ']' || *c == ',' || *c == '-' || *c == '/'
-        })
-        .collect();
-
+pub(crate) fn prepare_matrix_string(string: &str) -> Result<String, MathError> {
     // check if the matrix format is correct
-    let regex = match Regex::new(
-        r"^\[(\[((\-|/|[0-9])+,)*(\-|/|[0-9])+\],)*(\[((\-|/|[0-9])+,)*(\-|/|[0-9])+\])\]$",
-    ) {
-        Ok(regex) => regex,
-        _ => {
-            return Err(MathError::RegexError(
-                r"^\[(\[((\-|/|[0-9])+,)*(\-|/|[0-9])+\],)*(\[((\-|/|[0-9])+,)*(\-|/|[0-9])+\])\]$"
-                    .to_owned(),
-            ));
-        }
-    };
+    let regex =
+        match Regex::new(r"^\[(\[([^\[\],]+,)*[^\[\],]+\],)*(\[([^\[\],]+,)*[^\[\],]+\])\]$") {
+            Ok(regex) => regex,
+            _ => {
+                return Err(MathError::RegexError(
+                    r"^\[(\[([^\[\],]+,)*[^\[\],]+\],)*(\[([^\[\],]+,)*[^\[\],]+\])\]$".to_owned(),
+                ));
+            }
+        };
     // explanation of this regex:
     // it checks whether the string start with a '[' and ends with a ']'
     // we differ between the first/several and the last row (as there is no comma after the last row)
@@ -95,9 +65,13 @@ mod test_prepare_matrix_string {
     fn correct_matrix_work() {
         let matrix_string1 = String::from("[[1, 2, 3],[3, 4, 5]]");
         let matrix_string2 = String::from("[[1/3, -2/7, 3],[3, 4, -5/-2]]");
+        let matrix_string3 = String::from("[[4  0 1 2 3, 2  0 1],[1  5, 2  7 8]]");
+        let matrix_string4 = String::from("[[sdclin, =§&%, +57n4],[+dk<, 37 ffew, 8fh2n]]");
 
         assert!(prepare_matrix_string(&matrix_string1).is_ok());
         assert!(prepare_matrix_string(&matrix_string2).is_ok());
+        assert!(prepare_matrix_string(&matrix_string3).is_ok());
+        assert!(prepare_matrix_string(&matrix_string4).is_ok());
     }
 
     // Ensure that incorrect strings of a matrix are rejected.
@@ -105,9 +79,13 @@ mod test_prepare_matrix_string {
     fn incorrect_entries_error() {
         let matrix_string1 = String::from("[1, 2, 3],[3, 4, 5]");
         let matrix_string2 = String::from("[1/3, -2/7, 3,[3, 4, -5/-2]]");
+        let matrix_string3 = String::from("[1, [2], 3],[3, 4, 5]");
+        let matrix_string4 = String::from("[1, 2, 3][3, 4, 5]");
 
         assert!(prepare_matrix_string(&matrix_string1).is_err());
         assert!(prepare_matrix_string(&matrix_string2).is_err());
+        assert!(prepare_matrix_string(&matrix_string3).is_err());
+        assert!(prepare_matrix_string(&matrix_string4).is_err());
     }
 
     // Ensure that correct strings of a matrix are prepared correctly.
@@ -115,14 +93,24 @@ mod test_prepare_matrix_string {
     fn correct_matrix_format() {
         let matrix_string1 = String::from("[[1, 2, 3],[3, 4, 5]]");
         let matrix_string2 = String::from("[[1/3, -2/7, 3],[3, 4, -5/-2]]");
+        let matrix_string3 = String::from("[[4  0 1 2 3, 2  0 1],[1  5, 2  7 8]]");
+        let matrix_string4 = String::from("[[sdclin, =§&%, +57n4],[+dk<, 37 ffew, 8fh2n]]");
 
         assert_eq!(
             prepare_matrix_string(&matrix_string1).unwrap(),
-            "1,2,3|3,4,5"
+            "1, 2, 3|3, 4, 5"
         );
         assert_eq!(
             prepare_matrix_string(&matrix_string2).unwrap(),
-            "1/3,-2/7,3|3,4,-5/-2"
+            "1/3, -2/7, 3|3, 4, -5/-2"
+        );
+        assert_eq!(
+            prepare_matrix_string(&matrix_string3).unwrap(),
+            "4  0 1 2 3, 2  0 1|1  5, 2  7 8"
+        );
+        assert_eq!(
+            prepare_matrix_string(&matrix_string4).unwrap(),
+            "sdclin, =§&%, +57n4|+dk<, 37 ffew, 8fh2n"
         );
     }
 }
