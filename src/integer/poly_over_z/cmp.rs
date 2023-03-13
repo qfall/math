30354b6512718af18ccfe6d529ef6710feede945
Marvin Beckmann <116304267+Marvin-Beckmann@users.noreply.github.com>
2023-03-13 17:03:14 +0100

    Update src/integer/poly_over_z/cmp.rs
    Co-authored-by: MoogSven <113138151+MoogSven@users.noreply.github.com>

--- src/integer/poly_over_z/cmp.rs
+++ src/integer/poly_over_z/cmp.rs
@@ -6,7 +6,7 @@ use flint_sys::fmpz_poly::fmpz_poly_equal;
 use super::PolyOverZ;
 
 impl PartialEq for PolyOverZ {
-    /// Checks if two polynomials are equal. Used by the `==` and `!=` operators.
+    /// Checks if two polynomials over [`Z`] are equal. Used by the `==` and `!=` operators.
     ///
     /// Parameters:
     /// - other: the other value that is used to compare the elements
