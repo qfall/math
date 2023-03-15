86491cc27781aef34bec4d20b4a11327a43d5c5a
3pmile <pmile@mail.uni-paderborn.de>
2023-03-15 15:45:36 +0100

    applying review 3

--- src/rational/q/arithmetic/div.rs
+++ src/rational/q/arithmetic/div.rs
@@ -1,10 +1,13 @@
 //! Implementation of the [`Div`] trait for [`Q`] values.
 
 use super::super::Q;
-use crate::macros::arithmetics::{
-    arithmetic_trait_borrowed_to_owned, arithmetic_trait_mixed_borrowed_owned,
+use crate::{
+    error::MathError,
+    macros::arithmetics::{
+        arithmetic_trait_borrowed_to_owned, arithmetic_trait_mixed_borrowed_owned,
+    },
 };
-use flint_sys::fmpq::fmpq_div;
+use flint_sys::fmpq::{fmpq_div, fmpq_is_zero};
 use std::ops::Div;
 
 impl Div for &Q {
@@ -31,17 +34,43 @@ impl Div for &Q {
     /// let f: Q = c / &e;
     /// ```
     fn div(self, other: Self) -> Self::Output {
-        let mut out = Q::default();
-        unsafe {
-            fmpq_div(&mut out.value, &self.value, &other.value);
-        }
-        out
+        self.safe_div(other).unwrap()
     }
 }
 
 arithmetic_trait_borrowed_to_owned!(Div, div, Q);
 arithmetic_trait_mixed_borrowed_owned!(Div, div, Q);
 
+impl Q {
+    /// Implements division for two borrowed [`Q`] values.
+    ///
+    /// Parameters:
+    /// - `divisor`: specifies the value `self` is divided by.
+    ///
+    /// Returns the result ot the division as a [`Q`].
+    ///
+    /// # Example
+    /// ```
+    /// use math::rational::Q;
+    /// use std::str::FromStr;
+    ///
+    /// let a: Q = Q::from_str("42").unwrap();
+    /// let b: Q = Q::from_str("24").unwrap();
+    ///
+    /// let c: Q = a.safe_div(&b).unwrap();
+    /// ```
+    pub fn safe_div(&self, divisor: &Q) -> Result<Q, MathError> {
+        if 0 != unsafe { fmpq_is_zero(&divisor.value) } {
+            //todo replace: return Err(MathError::DivisionByZeroError(format!("\n tried to divide Q {} by Q {}", self.to_string(), divisor.to_string()).to_string()));
+            return Err(MathError::DivisionByZeroError("".to_string()));
+        }
+        let mut out = Q::default();
+        unsafe {
+            fmpq_div(&mut out.value, &self.value, &divisor.value);
+        }
+        Ok(out)
+    }
+}
 #[cfg(test)]
 mod test_div {
     use super::Q;
@@ -101,5 +130,22 @@ mod test_div {
             ))
             .unwrap()
         );
+    }
+
+    /// testing division by zero throws an error
+    #[test]
+    #[should_panic]
+    fn div_by_zero() {
+        let a: Q = Q::from_str("2").unwrap();
+        let b: Q = Q::from_str("0").unwrap();
+        let c = a / b;
+    }
+
+    /// testing division by zero throws an error
+    #[test]
+    fn div_by_zero_safe() {
+        let a: Q = Q::from_str("2").unwrap();
+        let b: Q = Q::from_str("0").unwrap();
+        assert!(&a.safe_div(&b).is_err());
     }
 }
