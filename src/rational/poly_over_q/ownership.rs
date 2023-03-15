0f023d87eca49e53bd150308cc08864af03576e0
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-03-15 13:45:58 +0100

    Revise free_memory test for Drop of PolyOverQ

--- src/rational/poly_over_q/ownership.rs
+++ src/rational/poly_over_q/ownership.rs
@@ -154,9 +154,12 @@ mod test_drop {
         let mut storage_addresses = HashSet::new();
 
         for _i in 0..5 {
-            storage_addresses.insert(create_and_drop_modulus());
+            let (a, b, c) = create_and_drop_modulus();
+            storage_addresses.insert(a);
+            storage_addresses.insert(b);
+            storage_addresses.insert(c);
         }
 
-        assert_ne!(storage_addresses.capacity(), 5);
+        assert!(storage_addresses.len() < 15);
     }
 }
