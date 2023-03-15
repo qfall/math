2ac8fce78e3a32dee4395ee1e89874a481e6bca3
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-03-15 10:09:03 +0100

    add traits for get coefficient

--- src/traits.rs
+++ src/traits.rs
@@ -1,6 +1,10 @@
 //! This module contains basic traits for this library. These include
 //! specific traits for matrices and polynomials.
 
+use std::fmt::Display;
+
+use crate::error::MathError;
+
 /// Is implemented by polynomials to evaluate it for a certain input.
 pub trait Evaluate<U, V> {
     /// Evaluates the object, e.g. polynomial or a matrix of polynomials,
@@ -11,4 +15,8 @@ pub trait Evaluate<U, V> {
     ///
     /// Returns the evaluation of the object.
     fn evaluate(&self, value: impl Into<U>) -> V;
+}
+
+pub trait GetCoefficient<T> {
+    fn get_coeff(&self, coordinate: impl TryInto<i64> + Display + Copy) -> Result<T, MathError>;
 }
