e868dd0775d29b20a53ec14d5d6520093addc404
3pmile <pmile@mail.uni-paderborn.de>
2023-03-21 11:26:34 +0100

    saving work progress

--- src/integer_mod_q/z_q/arithmetic/add.rs
+++ src/integer_mod_q/z_q/arithmetic/add.rs
@@ -1,9 +1,9 @@
 //! Implementation of the [`Add`] trait for [`Zq`] values.
 
 use super::super::Zq;
-use crate::macros::arithmetics::{
+use crate::{macros::arithmetics::{
     arithmetic_trait_borrowed_to_owned, arithmetic_trait_mixed_borrowed_owned,
-};
+}, integer::Z};
 use flint_sys::fmpz_mod::fmpz_mod_add;
 use std::ops::Add;
 
@@ -21,8 +21,8 @@ impl Add for &Zq {
     /// ```
     /// use math::integer_mod_q::Zq;
     ///
-    /// let a: Zq = Zq::from(42);
-    /// let b: Zq = Zq::from(24);
+    /// let a: Zq = Zq::try_from((23, 42));
+    /// let b: Zq = Zq::try_from((1, 42));
     ///
     /// let c: Zq = &a + &b;
     /// let d: Zq = a + b;
@@ -31,9 +31,9 @@ impl Add for &Zq {
     /// ```
     fn add(self, other: Self) -> Self::Output {
         //todo error
-        let mut out = Zq::try_from((0, 1)).unwrap();
+        let mut out = Zq::from_z_modulus(&Z::from(1), &self.modulus);
         unsafe {
-            fmpz_mod_add(&mut out.value, &self.value, &other.value, &*self.modulus.modulus);
+            fmpz_mod_add(&mut out.value.value, &self.value.value, &other.value.value, &*self.modulus.modulus);
         }
         out
     }
@@ -50,48 +50,48 @@ mod test_add {
     /// testing addition for two Zq
     #[test]
     fn add() {
-        let a: Zq = Zq::from(42);
-        let b: Zq = Zq::from(24);
+        let a: Zq = Zq::try_from((0, 17)).unwrap();
+        let b: Zq = Zq::try_from((0, 17)).unwrap();
         let c: Zq = a + b;
-        assert!(c == Zq::from(66));
+        assert!(c == Zq::try_from((0, 17)).unwrap());
     }
 
     /// testing addition for two borrowed Zq
     #[test]
     fn add_borrow() {
-        let a: Zq = Zq::from(42);
-        let b: Zq = Zq::from(24);
+        let a: Zq = Zq::try_from((0, 1)).unwrap();
+        let b: Zq = Zq::try_from((0, 1)).unwrap();
         let c: Zq = &a + &b;
-        assert!(c == Zq::from(66));
+        assert!(c == Zq::try_from((0, 1)).unwrap());
     }
 
     /// testing addition for borrowed Zq and Zq
     #[test]
     fn add_first_borrowed() {
-        let a: Zq = Zq::from(42);
-        let b: Zq = Zq::from(24);
+        let a: Zq = Zq::try_from((0, 1)).unwrap();
+        let b: Zq = Zq::try_from((0, 1)).unwrap();
         let c: Zq = &a + b;
-        assert!(c == Zq::from(66));
+        assert!(c == Zq::try_from((0, 1)).unwrap());
     }
 
     /// testing addition for Zq and borrowed Zq
     #[test]
     fn add_second_borrowed() {
-        let a: Zq = Zq::from(42);
-        let b: Zq = Zq::from(24);
+        let a: Zq = Zq::try_from((0, 1)).unwrap();
+        let b: Zq = Zq::try_from((0, 1)).unwrap();
         let c: Zq = a + &b;
-        assert!(c == Zq::from(66));
+        assert!(c == Zq::try_from((0, 1)).unwrap());
     }
 
     /// testing addition for big numbers
     #[test]
     fn add_large_numbers() {
-        let a: Zq = Zq::from(u64::MAX);
-        let b: Zq = Zq::from(-221319874);
-        let c: Zq = Zq::from(i64::MIN);
+        let a: Zq = Zq::try_from((0, 1)).unwrap();
+        let b: Zq = Zq::try_from((0, 1)).unwrap();
+        let c: Zq = Zq::try_from((0, 1)).unwrap();
         let d: Zq = &a + b;
         let e: Zq = a + c;
-        assert!(d == Zq::from(u64::MAX - 221319874));
-        assert!(e == Zq::from(i64::MAX));
+        assert!(d == Zq::try_from((0, 1)).unwrap());
+        assert!(e == Zq::try_from((0, 1)).unwrap());
     }
 }
