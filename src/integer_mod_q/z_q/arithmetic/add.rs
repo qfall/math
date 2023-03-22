28a16f90306f435ec6d365508b66677ab6280812
3pmile <pmile@mail.uni-paderborn.de>
2023-03-22 13:12:47 +0100

    saving progress

--- src/integer_mod_q/z_q/arithmetic/add.rs
+++ src/integer_mod_q/z_q/arithmetic/add.rs
@@ -1,9 +1,13 @@
 //! Implementation of the [`Add`] trait for [`Zq`] values.
 
 use super::super::Zq;
-use crate::{macros::arithmetics::{
-    arithmetic_trait_borrowed_to_owned, arithmetic_trait_mixed_borrowed_owned,
-}, integer::Z};
+use crate::{
+    error::MathError,
+    integer::Z,
+    macros::arithmetics::{
+        arithmetic_trait_borrowed_to_owned, arithmetic_trait_mixed_borrowed_owned,
+    },
+};
 use flint_sys::fmpz_mod::fmpz_mod_add;
 use std::ops::Add;
 
@@ -21,21 +25,59 @@ impl Add for &Zq {
     /// ```
     /// use math::integer_mod_q::Zq;
     ///
-    /// let a: Zq = Zq::try_from((23, 42));
-    /// let b: Zq = Zq::try_from((1, 42));
+    /// let a: Zq = Zq::try_from((23, 42)).unwrap();
+    /// let b: Zq = Zq::try_from((1, 42)).unwrap();
     ///
     /// let c: Zq = &a + &b;
     /// let d: Zq = a + b;
     /// let e: Zq = &c + d;
     /// let f: Zq = c + &e;
     /// ```
+    ///
+    /// # Panics
+    /// - Panics if the modulus of both [`Zq`] mismatches.
     fn add(self, other: Self) -> Self::Output {
-        //todo error
+        self.add_safe(other).unwrap()
+    }
+}
+
+impl Zq {
+    /// Implements addition for two [`Zq`] values.
+    ///
+    ///
+    /// Parameters:
+    /// - `other`: specifies the value to add to `self`
+    ///
+    /// Returns the sum of both numbers as a [`Zq`] or an error if the modulus
+    /// do mismatch.
+    ///
+    /// # Example
+    /// ```
+    /// use math::integer_mod_q::Zq;
+    ///
+    /// let a: Zq = Zq::try_from((23, 42)).unwrap();
+    /// let b: Zq = Zq::try_from((1, 42)).unwrap();
+    ///
+    /// let c: Zq = a.add_safe(&b).unwrap();
+
+    /// ```
+    /// # Errors
+    /// Returns a [`MathError`] of type [`MathError::Error`] if the modulus of
+    /// both [`Zq`] mismatches.
+    pub fn add_safe(&self, other: &Self) -> Result<Zq, MathError> {
+        if self.modulus != other.modulus {
+            return Err(MathError::DivisionByZeroError("".to_string())); //TODO
+        }
         let mut out = Zq::from_z_modulus(&Z::from(1), &self.modulus);
         unsafe {
-            fmpz_mod_add(&mut out.value.value, &self.value.value, &other.value.value, &*self.modulus.modulus);
+            fmpz_mod_add(
+                &mut out.value.value,
+                &self.value.value,
+                &other.value.value,
+                &*self.modulus.modulus,
+            );
         }
-        out
+        Ok(out)
     }
 }
 
@@ -47,51 +89,57 @@ mod test_add {
 
     use super::Zq;
 
-    /// testing addition for two Zq
+    /// testing addition for two [`Zq`]
     #[test]
     fn add() {
-        let a: Zq = Zq::try_from((0, 17)).unwrap();
-        let b: Zq = Zq::try_from((0, 17)).unwrap();
+        let a: Zq = Zq::try_from((11, 17)).unwrap();
+        let b: Zq = Zq::try_from((12, 17)).unwrap();
         let c: Zq = a + b;
-        assert!(c == Zq::try_from((0, 17)).unwrap());
+        assert!(c == Zq::try_from((6, 17)).unwrap());
     }
 
-    /// testing addition for two borrowed Zq
+    /// testing addition for two borrowed [`Zq`]
     #[test]
     fn add_borrow() {
-        let a: Zq = Zq::try_from((0, 1)).unwrap();
-        let b: Zq = Zq::try_from((0, 1)).unwrap();
+        let a: Zq = Zq::try_from((10, 11)).unwrap();
+        let b: Zq = Zq::try_from((1, 11)).unwrap();
         let c: Zq = &a + &b;
-        assert!(c == Zq::try_from((0, 1)).unwrap());
+        assert!(c == Zq::try_from((0, 11)).unwrap());
     }
 
-    /// testing addition for borrowed Zq and Zq
+    /// testing addition for borrowed [`Zq`] and [`Zq`]
     #[test]
     fn add_first_borrowed() {
-        let a: Zq = Zq::try_from((0, 1)).unwrap();
-        let b: Zq = Zq::try_from((0, 1)).unwrap();
+        let a: Zq = Zq::try_from((2, 11)).unwrap();
+        let b: Zq = Zq::try_from((5, 11)).unwrap();
         let c: Zq = &a + b;
-        assert!(c == Zq::try_from((0, 1)).unwrap());
+        assert!(c == Zq::try_from((7, 11)).unwrap());
     }
 
-    /// testing addition for Zq and borrowed Zq
+    /// testing addition for [`Zq`] and borrowed [`Zq`]
     #[test]
     fn add_second_borrowed() {
-        let a: Zq = Zq::try_from((0, 1)).unwrap();
-        let b: Zq = Zq::try_from((0, 1)).unwrap();
+        let a: Zq = Zq::try_from((12, 11)).unwrap();
+        let b: Zq = Zq::try_from((10, 11)).unwrap();
         let c: Zq = a + &b;
-        assert!(c == Zq::try_from((0, 1)).unwrap());
+        assert!(c == Zq::try_from((0, 11)).unwrap());
     }
 
-    /// testing addition for big numbers
+    /// testing addition for big [`Zq`]
     #[test]
     fn add_large_numbers() {
-        let a: Zq = Zq::try_from((0, 1)).unwrap();
-        let b: Zq = Zq::try_from((0, 1)).unwrap();
-        let c: Zq = Zq::try_from((0, 1)).unwrap();
-        let d: Zq = &a + b;
-        let e: Zq = a + c;
-        assert!(d == Zq::try_from((0, 1)).unwrap());
-        assert!(e == Zq::try_from((0, 1)).unwrap());
+        let a: Zq = Zq::try_from((u32::MAX, u32::MAX - 58)).unwrap();
+        let b: Zq = Zq::try_from((i32::MAX, u32::MAX - 58)).unwrap();
+        let c: Zq = a + b;
+        assert!(c == Zq::try_from(((u32::MAX - 1) / 2 + 58, u32::MAX - 58)).unwrap());
+    }
+
+    /// testing addition for [`Zq`] with different modulus does not work
+    #[test]
+    #[should_panic]
+    fn add_mismatching_modulus() {
+        let a: Zq = Zq::try_from((4, 11)).unwrap();
+        let b: Zq = Zq::try_from((1, 3)).unwrap();
+        let _c: Zq = a + b;
     }
 }
