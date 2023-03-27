84e2052e17e4b073ac7e24380e75bdddace5da34
Marcel Schmidt <marcel.schmidtfuldatal@gmail.com>
2023-03-27 17:11:51 +0200

    implemented to_string for MatQ

---
+++ src/rational/mat_q/to_string.rs
@@ -0,0 +1,120 @@
+// Copyright © 2023 Marcel Luca Schmidt
+//
+// This file is part of qFALL-math.
+//
+// qFALL-math is free software: you can redistribute it and/or modify it under
+// the terms of the Mozilla Public License Version 2.0 as published by the
+// Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
+
+//! This module contains all options to convert a matrix of type
+//! [`MatQ`] into a [`String`].
+//!
+//! This includes the [`Display`](std::fmt::Display) trait.
+
+use crate::utils::parse::matrix_to_string;
+
+use super::MatQ;
+use core::fmt;
+
+impl fmt::Display for MatQ {
+    /// Allows to convert a matrix of type [`MatQ`] into a [`String`].
+    ///
+    /// Returns the Matrix in form of a [`String`]. For matrix `[[1/2, 2, 3/4],[4, 5/3, 6/2]]`
+    /// the String looks like this `[[1/2, 2, 3/4],[4, 5/3, 3]]`.
+    ///
+    /// # Examples
+    /// ```
+    /// use math::rational::MatQ;
+    /// use core::fmt;
+    /// use std::str::FromStr;
+    ///
+    /// let matrix = MatQ::from_str("[[1/2,2,3/4],[4,5/3,6]]").unwrap();
+    /// println!("{}", matrix);
+    /// ```
+    ///
+    /// ```
+    /// use math::rational::MatQ;
+    /// use core::fmt;
+    /// use std::str::FromStr;
+    ///
+    /// let matrix = MatQ::from_str("[[1/2,2,3/4],[4,5/3,6]]").unwrap();
+    /// let matrix_string = matrix.to_string();
+    /// ```
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", matrix_to_string(self))
+    }
+}
+
+#[cfg(test)]
+mod test_to_string {
+    use crate::rational::MatQ;
+    use std::str::FromStr;
+
+    /// tests whether a matrix with large nominators and denominators works in a
+    /// roundtrip
+    #[test]
+    fn working_large_positive() {
+        let cmp =
+            MatQ::from_str(&format!("[[{}, 1/{}, 3],[5, 6, 7]]", u64::MAX, u64::MAX)).unwrap();
+
+        assert_eq!(
+            format!("[[{}, 1/{}, 3],[5, 6, 7]]", u64::MAX, u64::MAX),
+            cmp.to_string()
+        )
+    }
+
+    /// tests whether a matrix with large negative nominators and denominators
+    /// works in a roundtrip
+    #[test]
+    fn working_large_negative() {
+        let cmp =
+            MatQ::from_str(&format!("[[-{}, 1/-{}, 3],[5, 6, 7]]", u64::MAX, u64::MAX)).unwrap();
+
+        assert_eq!(
+            format!("[[-{}, -1/{}, 3],[5, 6, 7]]", u64::MAX, u64::MAX),
+            cmp.to_string()
+        )
+    }
+
+    /// tests whether a matrix with positive nominators and denominators works
+    /// in a roundtrip
+    #[test]
+    fn working_positive() {
+        let cmp = MatQ::from_str("[[2, 1, 2/3],[5, 6, 14/7]]").unwrap();
+
+        assert_eq!("[[2, 1, 2/3],[5, 6, 2]]", cmp.to_string())
+    }
+
+    /// tests whether a matrix with negative nominators and denominators works
+    /// in a roundtrip
+    #[test]
+    fn working_negative() {
+        let cmp = MatQ::from_str("[[-2, 1, -3/8],[5, 1/-6, -14/7]]").unwrap();
+
+        assert_eq!("[[-2, 1, -3/8],[5, -1/6, -2]]", cmp.to_string())
+    }
+
+    /// tests whether a large matrix works in a roundtrip
+    #[test]
+    fn working_big_dimensions() {
+        let cmp1 = MatQ::from_str(&format!("[{}[5, 6, 7]]", "[1/2, 2, 3/8],".repeat(99))).unwrap();
+        let cmp2 = MatQ::from_str(&format!("[[{}1]]", "1/4, ".repeat(99))).unwrap();
+
+        assert_eq!(
+            format!("[{}[5, 6, 7]]", "[1/2, 2, 3/8],".repeat(99)),
+            cmp1.to_string()
+        );
+        assert_eq!(format!("[[{}1]]", "1/4, ".repeat(99)), cmp2.to_string());
+    }
+
+    /// tests whether a matrix that is created using a string, returns a
+    /// string that can be used to create a [`MatQ`]
+    #[test]
+    fn working_use_result_of_to_string_as_input() {
+        let cmp = MatQ::from_str("[[-2/5, 1, 3],[5, 1/-6, 14/7]]").unwrap();
+
+        let cmp_string2 = cmp.to_string();
+
+        assert!(MatQ::from_str(&cmp_string2).is_ok())
+    }
+}
