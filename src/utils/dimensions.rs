15698640ec8928ebc55d08df13e25501acc1e6ad
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-03-29 13:40:53 +0200

    MatZ Mul, Cmp, Transpose (#115)
    * Add macros for cross type arithmetics
    
    * Implement multiplication for MatZ
    
    * Implement PartialEq for MatZ
    
    * Implement transpose for MatZ
    
    * Add VecZ
    
    * Implement MatZ * VecZ + tests
    
    * Add instantiation methods for VecZ
    
    * Add getter and transpose to VecZ
    
    * Implement multiplication for VecZ in combination with MatZ
    
    * Apply requested PR changes and add from_str tests for VecZ
    
    * Apply rustfmt + add import of GetEntry for VecZ
    
    * Make vector viable as Column and RowVector
    
    * Apply requested PR changes to VecZ
    
    * Remove vectors

--- src/utils/dimensions.rs
+++ src/utils/dimensions.rs
@@ -6,7 +6,7 @@
 // the terms of the Mozilla Public License Version 2.0 as published by the
 // Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
 
-//! Implements methods for finding matrix dimensions.
+//! Implements methods for finding matrix dimensions and enums for detecting vector directions.
 
 use crate::error::MathError;
 
