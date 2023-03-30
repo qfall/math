29bb7debcd43888cb605977c2fd9d3415227b0ce
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-03-30 22:39:34 +0200

    Implement dot_product for MatZ vectors

--- src/integer/mat_z/get.rs
+++ src/integer/mat_z/get.rs
@@ -95,6 +95,33 @@ impl GetEntry<Z> for MatZ {
     }
 }
 
+impl MatZ {
+    /// Efficiently collects all [`fmpz`]s in a [`MatZ`] without cloning them.
+    ///
+    /// # Example
+    /// ```compile_fail
+    /// use math::intger::MatZ;
+    /// use std::str::FromStr;
+    ///
+    /// let mat = MatZ::from_str("[[1,2],[3,4],[5,6]]").unwrap();
+    ///
+    /// let fmpz_entries = mat.collect_entries();
+    /// ```
+    pub(crate) fn collect_entries(&self) -> Vec<fmpz> {
+        let mut entries: Vec<fmpz> = vec![];
+
+        for row in 0..self.get_num_rows() {
+            for col in 0..self.get_num_columns() {
+                // efficiently get entry without cloning the entry itself
+                let entry = unsafe { *fmpz_mat_entry(&self.matrix, row, col) };
+                entries.push(entry);
+            }
+        }
+
+        entries
+    }
+}
+
 #[cfg(test)]
 mod test_get_entry {
 
@@ -219,5 +246,33 @@ mod test_get_num {
         let matrix = MatZ::new(5, 10).unwrap();
 
         assert_eq!(matrix.get_num_columns(), 10);
+    }
+}
+
+#[cfg(test)]
+mod test_collect_entries {
+    use super::MatZ;
+    use std::str::FromStr;
+
+    #[test]
+    fn all_entries_collected() {
+        let mat_1 = MatZ::from_str(&format!("[[1,2],[{},{}],[3,4]]", i64::MAX, i64::MIN)).unwrap();
+        let mat_2 = MatZ::from_str("[[-1,2]]").unwrap();
+
+        let entries_1 = mat_1.collect_entries();
+        let entries_2 = mat_2.collect_entries();
+
+        assert_eq!(entries_1.len(), 6);
+        assert_eq!(entries_1[0].0, 1);
+        assert_eq!(entries_1[1].0, 2);
+        // 4611686018427387904 = 2^62, i.e. value is stored on stack
+        assert!(entries_1[2].0 >= 4611686018427387904);
+        assert!(entries_1[3].0 >= 4611686018427387904);
+        assert_eq!(entries_1[4].0, 3);
+        assert_eq!(entries_1[5].0, 4);
+
+        assert_eq!(entries_2.len(), 2);
+        assert_eq!(entries_2[0].0, -1);
+        assert_eq!(entries_2[1].0, 2);
     }
 }
---
+++ src/integer/mat_z/vector/dot_product.rs
@@ -0,0 +1,164 @@
+// Copyright © 2023 Niklas Siemer
+//
+// This file is part of qFALL-math.
+//
+// qFALL-math is free software: you can redistribute it and/or modify it under
+// the terms of the Mozilla Public License Version 2.0 as published by the
+// Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
+
+//! This module includes functionality to compute the dot product of two vectors.
+
+use crate::error::MathError;
+use crate::integer::{MatZ, Z};
+use crate::traits::{GetNumColumns, GetNumRows};
+use flint_sys::fmpz::{fmpz, fmpz_addmul};
+
+impl MatZ {
+    /// Returns the dot product of two vectors of type [`MatZ`].
+    ///
+    /// # Example
+    /// ```
+    /// use math::integer::MatZ;
+    /// use std::str::FromStr;
+    /// # use math::integer::Z;
+    ///
+    /// let vec_1 = MatZ::from_str("[[1],[2],[3]]").unwrap();
+    /// let vec_2 = MatZ::from_str("[[1,3,2]]").unwrap();
+    ///
+    /// let dot_prod = vec_1.dot_product(&vec_2).unwrap();
+    ///
+    /// assert_eq!(Z::from_i64(13), dot_prod);
+    /// ```
+    ///
+    /// Errors and Failures
+    /// - Returns a [`MathError`] of type [`MathError::VectorFunctionCalledOnNonVector`] if
+    /// the given [`MatZ`] instance is not a (row or column) vector.
+    /// - Returns a [`MathError`] of type [`MathError::MismatchingVectorDimensions`] if
+    /// the given vectors have different lengths.
+    pub fn dot_product(&self, other: &Self) -> Result<Z, MathError> {
+        if !self.is_vector() {
+            return Err(MathError::VectorFunctionCalledOnNonVector(
+                String::from("dot_product"),
+                self.get_num_rows(),
+                self.get_num_columns(),
+            ));
+        } else if !other.is_vector() {
+            return Err(MathError::VectorFunctionCalledOnNonVector(
+                String::from("dot_product"),
+                other.get_num_rows(),
+                other.get_num_columns(),
+            ));
+        }
+
+        let self_entries = self.collect_entries();
+        let other_entries = other.collect_entries();
+
+        if self_entries.len() != other_entries.len() {
+            return Err(MathError::MismatchingVectorDimensions(format!(
+                "You called the function 'dot_product' for vectors of different lengths: {} and {}",
+                self_entries.len(),
+                other_entries.len()
+            )));
+        }
+
+        // calculate dot product of vectors
+        let mut result = fmpz(0);
+        for i in 0..self_entries.len() {
+            // sets result = result + self.entry[i] * other.entry[i] without cloned Z element
+            unsafe { fmpz_addmul(&mut result, &self_entries[i], &other_entries[i]) }
+        }
+
+        Ok(Z { value: result })
+    }
+}
+
+#[cfg(test)]
+mod test_dot_product {
+
+    use super::{MatZ, Z};
+    use std::str::FromStr;
+
+    /// Check whether the dot product is calculated correctly for the combination:
+    /// `self`: row vector, `other`: row vector
+    #[test]
+    fn row_with_row() {
+        let vec_1 = MatZ::from_str("[[1,2,-3]]").unwrap();
+        let vec_2 = MatZ::from_str("[[1,3,2]]").unwrap();
+
+        let dot_prod = vec_1.dot_product(&vec_2).unwrap();
+
+        assert_eq!(dot_prod, Z::ONE);
+    }
+
+    /// Check whether the dot product is calculated correctly for the combination:
+    /// `self`: column vector, `other`: column vector
+    #[test]
+    fn column_with_column() {
+        let vec_1 = MatZ::from_str("[[1],[2],[-3]]").unwrap();
+        let vec_2 = MatZ::from_str("[[1],[3],[2]]").unwrap();
+
+        let dot_prod = vec_1.dot_product(&vec_2).unwrap();
+
+        assert_eq!(dot_prod, Z::ONE);
+    }
+
+    /// Check whether the dot product is calculated correctly for the combination:
+    /// `self`: row vector, `other`: column vector
+    #[test]
+    fn row_with_column() {
+        let vec_1 = MatZ::from_str("[[1,2,-3]]").unwrap();
+        let vec_2 = MatZ::from_str("[[1],[3],[2]]").unwrap();
+
+        let dot_prod = vec_1.dot_product(&vec_2).unwrap();
+
+        assert_eq!(dot_prod, Z::ONE);
+    }
+
+    /// Check whether the dot product is calculated correctly for the combination:
+    /// `self`: column vector, `other`: row vector
+    #[test]
+    fn column_with_row() {
+        let vec_1 = MatZ::from_str("[[1],[2],[-3]]").unwrap();
+        let vec_2 = MatZ::from_str("[[1,3,2]]").unwrap();
+
+        let dot_prod = vec_1.dot_product(&vec_2).unwrap();
+
+        assert_eq!(dot_prod, Z::ONE);
+    }
+
+    /// Check whether the dot product is calculated correctly with large numbers
+    #[test]
+    fn large_numbers() {
+        let vec_1 = MatZ::from_str(&format!("[[1,-1,{}]]", i64::MAX)).unwrap();
+        let vec_2 = MatZ::from_str(&format!("[[1,{},1]]", i64::MIN)).unwrap();
+        let cmp = Z::from(-1) * Z::from(i64::MIN) + Z::from(i64::MAX) + Z::ONE;
+
+        let dot_prod = vec_1.dot_product(&vec_2).unwrap();
+
+        assert_eq!(dot_prod, cmp);
+    }
+
+    /// Check whether the dot product calculation on
+    /// non vector instances yield an error
+    #[test]
+    fn non_vector_yield_error() {
+        let vec = MatZ::from_str("[[1,3,2]]").unwrap();
+        let mat = MatZ::from_str("[[1,2],[2,3],[-3,4]]").unwrap();
+
+        assert!(vec.dot_product(&mat).is_err());
+        assert!(mat.dot_product(&vec).is_err());
+        assert!(mat.dot_product(&mat).is_err());
+        assert!(vec.dot_product(&vec).is_ok());
+    }
+
+    /// Check whether the dot product calculation on
+    /// vectors of different lengths yield an error
+    #[test]
+    fn different_lengths_yield_error() {
+        let vec_1 = MatZ::from_str("[[1,2,3]]").unwrap();
+        let vec_2 = MatZ::from_str("[[1,2,3,4]]").unwrap();
+
+        assert!(vec_1.dot_product(&vec_2).is_err());
+        assert!(vec_2.dot_product(&vec_1).is_err());
+    }
+}
