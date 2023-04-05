6b99494bb0710f28d66c5dfb732c8b5d382326d4
Marcel Schmidt <marcel.schmidtfuldatal@gmail.com>
2023-04-05 11:30:02 +0200

    implemented scalar mul for MatZ

---
+++ src/integer/mat_z/arithmetic/mul_scalar.rs
@@ -0,0 +1,169 @@
+// Copyright © 2023 Marcel Luca Schmidt
+//
+// This file is part of qFALL-math.
+//
+// qFALL-math is free software: you can redistribute it and/or modify it under
+// the terms of the Mozilla Public License Version 2.0 as published by the
+// Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
+
+//! Implementation of scalar multiplication for [`MatZ`] matrices.
+
+use super::super::MatZ;
+use crate::integer::Z;
+use crate::macros::arithmetics::{
+    arithmetic_trait_borrowed_to_owned, arithmetic_trait_mixed_borrowed_owned,
+};
+use crate::traits::*;
+use flint_sys::fmpz_mat::fmpz_mat_scalar_mul_fmpz;
+use std::ops::Mul;
+
+impl Mul<&Z> for &MatZ {
+    type Output = MatZ;
+    /// Implements multiplication for a [`MatZ`] matrix with a [`Z`] integer.
+    ///
+    /// Parameters:
+    /// - `scalar`: specifies the integer value by which the matrix is multiplied
+    ///
+    /// Returns the product of `self` and `other` as a [`MatZ`].
+    ///
+    /// # Example
+    /// ```
+    /// use qfall_math::integer::MatZ;
+    /// use qfall_math::integer::Z;
+    ///  use std::str::FromStr;
+    ///
+    /// let mat1 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+    /// let integer = Z::from(3);
+    ///
+    /// let mat2 = &mat1 * &integer;
+    /// ```
+    fn mul(self, other: &Z) -> Self::Output {
+        let mut out = MatZ::new(self.get_num_rows(), self.get_num_columns()).unwrap();
+        unsafe {
+            fmpz_mat_scalar_mul_fmpz(&mut out.matrix, &self.matrix, &other.value);
+        }
+        out
+    }
+}
+
+impl Mul<&MatZ> for &Z {
+    type Output = MatZ;
+    /// Implements multiplication for a [`Z`] integer with a [`MatZ`] matrix.
+    ///
+    /// Parameters:
+    /// - `scalar`: specifies the integer value by which the matrix is multiplied
+    ///
+    /// Returns the product of `self` and `other` as a [`MatZ`].
+    ///
+    /// # Example
+    /// ```
+    /// use qfall_math::integer::MatZ;
+    /// use qfall_math::integer::Z;
+    /// use std::str::FromStr;
+    ///
+    /// let mat1 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+    /// let integer = Z::from(3);
+    ///
+    /// let mat2 = &integer * &mat1 ;
+    /// ```
+    fn mul(self, other: &MatZ) -> Self::Output {
+        let mut out = MatZ::new(other.get_num_rows(), other.get_num_columns()).unwrap();
+        unsafe {
+            fmpz_mat_scalar_mul_fmpz(&mut out.matrix, &other.matrix, &self.value);
+        }
+        out
+    }
+}
+
+arithmetic_trait_borrowed_to_owned!(Mul, mul, MatZ, Z, MatZ);
+arithmetic_trait_borrowed_to_owned!(Mul, mul, Z, MatZ, MatZ);
+arithmetic_trait_mixed_borrowed_owned!(Mul, mul, MatZ, Z, MatZ);
+arithmetic_trait_mixed_borrowed_owned!(Mul, mul, Z, MatZ, MatZ);
+
+#[cfg(test)]
+mod test_mul {
+
+    use super::MatZ;
+    use crate::integer::Z;
+    use std::str::FromStr;
+
+    /// Checks if matrix multiplication works fine for both borrowed
+    #[test]
+    fn borrowed_correctness() {
+        let mat1 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+        let mat2 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+        let mat3 = MatZ::from_str("[[6,3],[3,6]]").unwrap();
+        let integer = Z::from(3);
+
+        let mat1 = &mat1 * &integer;
+        let mat2 = &mat2 * &integer;
+
+        assert_eq!(mat3, mat1);
+        assert_eq!(mat3, mat2);
+    }
+
+    /// Checks if scalar multiplication works fine for both owned
+    #[test]
+    fn owned_correctness() {
+        let mat1 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+        let mat2 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+        let mat3 = MatZ::from_str("[[6,3],[3,6]]").unwrap();
+        let integer1 = Z::from(3);
+        let integer2 = Z::from(3);
+
+        let mat1 = mat1 * integer1;
+        let mat2 = integer2 * mat2;
+
+        assert_eq!(mat3, mat1);
+        assert_eq!(mat3, mat2);
+    }
+
+    /// Checks if scalar multiplication works fine for half owned/borrowed
+    #[test]
+    fn half_correctness() {
+        let mat1 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+        let mat2 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+        let mat3 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+        let mat4 = MatZ::from_str("[[2,1],[1,2]]").unwrap();
+        let mat5 = MatZ::from_str("[[6,3],[3,6]]").unwrap();
+        let integer1 = Z::from(3);
+        let integer2 = Z::from(3);
+
+        let mat1 = mat1 * &integer1;
+        let mat2 = &integer2 * mat2;
+        let mat3 = &mat3 * integer1;
+        let mat4 = integer2 * &mat4;
+
+        assert_eq!(mat5, mat1);
+        assert_eq!(mat5, mat2);
+        assert_eq!(mat5, mat3);
+        assert_eq!(mat5, mat4);
+    }
+
+    /// Checks if scalar multiplication works fine for matrices of different dimensions
+    #[test]
+    fn different_dimensions_correctness() {
+        let mat1 = MatZ::from_str("[[1],[0],[4]]").unwrap();
+        let mat2 = MatZ::from_str("[[2,5,6],[1,3,1]]").unwrap();
+        let mat3 = MatZ::from_str("[[3],[0],[12]]").unwrap();
+        let mat4 = MatZ::from_str("[[6,15,18],[3,9,3]]").unwrap();
+        let integer = Z::from(3);
+
+        assert_eq!(mat3, &integer * mat1);
+        assert_eq!(mat4, integer * mat2);
+    }
+
+    /// Checks if matrix multiplication works fine for large values
+    #[test]
+    fn large_entries() {
+        let mat1 = MatZ::from_str(&format!("[[1],[{}],[4]]", i64::MAX)).unwrap();
+        let mat2 = MatZ::from_str("[[3]]").unwrap();
+        let mat3 = MatZ::from_str(&format!("[[3],[{}],[12]]", 3 * i64::MAX as i128)).unwrap();
+        let mat4 = MatZ::from_str(&format!("[[{}]]", 3 * i64::MAX as i128)).unwrap();
+        let integer1 = Z::from(3);
+        let integer2 = Z::from(i64::MAX);
+
+        assert_eq!(mat3, integer1 * mat1);
+        assert_eq!(mat4, integer2 * mat2);
+    }
+}
