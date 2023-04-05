fcf526efec14cae4f917382abd092555f5670eff
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-04-05 12:29:50 +0200

    Implement constants ZERO + ONE for Q

--- src/rational/q/default.rs
+++ src/rational/q/default.rs
@@ -1,4 +1,4 @@
-// Copyright © 2023 Marcel Luca Schmidt
+// Copyright © 2023 Niklas Siemer
 //
 // This file is part of qFALL-math.
 //
@@ -6,7 +6,7 @@
 // the terms of the Mozilla Public License Version 2.0 as published by the
 // Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
 
-//! Default value for a [`Q`].
+//! Define default values for [`Q`].
 
 use super::Q;
 use flint_sys::{fmpq::fmpq, fmpz::fmpz};
@@ -14,7 +14,7 @@ use flint_sys::{fmpq::fmpq, fmpz::fmpz};
 impl Default for Q {
     /// Returns an instantiation of [`Q`] with value '0/1'.
     ///
-    /// # Example:
+    /// # Example
     /// ```
     /// use std::default::Default;
     /// use qfall_math::rational::Q;
@@ -22,26 +22,57 @@ impl Default for Q {
     /// let a: Q = Q::default();
     /// ```
     fn default() -> Self {
-        // TODO use from
-        Q {
-            value: fmpq {
-                num: fmpz(0),
-                den: fmpz(1),
-            },
+        Q::ZERO
         }
     }
+
+impl Q {
+    /// Returns an instantiation of [`Q`] with value `1`.
+    ///
+    /// # Example
+    /// ```
+    /// use math::rational::Q;
+    ///  
+    /// let a: Q = Q::ONE;
+    /// ```
+    pub const ONE: Q = Q {
+        value: fmpq { num: fmpz(1), den: fmpz(1) },
+    };
+
+    /// Returns an instantiation of [`Q`] with value `0`.
+    ///
+    /// # Example
+    /// ```
+    /// use math::rational::Q;
+    ///  
+    /// let a: Q = Q::ZERO;
+    /// ```
+    pub const ZERO: Q = Q {
+        value: fmpq { num: fmpz(0), den: fmpz(1) },
+    };
 }
 
 #[cfg(test)]
 mod tests_init {
-
     use super::Q;
-    use flint_sys::{
-        fmpq::{fmpq, fmpq_equal},
-        fmpz::fmpz,
-    };
 
-    /// TODO add cmp test
+    /// Ensure that [`Default`] initializes [`Q`] with `0`.
+    #[test]
+    fn init_default() {
+        assert_eq!(Q::ZERO, Q::default());
+    }
+
+    /// Ensure that `ZERO` initializes [`Q`] with `0`.
+    #[test]
+    fn init_zero() {
+        assert_eq!(Q::try_from((&0, &1)).unwrap(), Q::ZERO);
+    }
+
+    /// Ensure that `ONE` initializes [`Q`] with `1`.
+    #[test]
+    fn init_one() {
+        assert_eq!(Q::try_from((&1, &1)).unwrap(), Q::ONE);
+    }
 
     /// Ensure that initialization of default value works.
     #[test]
