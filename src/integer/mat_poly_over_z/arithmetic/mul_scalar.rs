18308b6608bb9b7e316954f159c0888a1ab06616
3pmile <pmile@mail.uni-paderborn.de>
2023-04-18 21:50:28 +0200

    fixing doc warnings

--- src/integer/mat_poly_over_z/arithmetic/mul_scalar.rs
+++ src/integer/mat_poly_over_z/arithmetic/mul_scalar.rs
@@ -6,7 +6,7 @@
 // the terms of the Mozilla Public License Version 2.0 as published by the
 // Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
 
-//! Implementation of scalar multiplication for [`MatZ`] matrices.
+//! Implementation of scalar multiplication for [`MatPolyOverZ`] matrices.
 
 use super::super::MatPolyOverZ;
 use crate::integer::{PolyOverZ, Z};
