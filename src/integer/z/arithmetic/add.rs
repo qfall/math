120a6709c5ec746223e7a8f27c50e1722a8f034a
3pmile <pmile@mail.uni-paderborn.de>
2023-04-25 13:17:04 +0200

    add Z+Q

--- src/integer/z/arithmetic/add.rs
+++ src/integer/z/arithmetic/add.rs
@@ -9,11 +9,14 @@
 //! Implementation of the [`Add`] trait for [`Z`] values.
 
 use super::super::Z;
-use crate::macros::arithmetics::{
-    arithmetic_between_types, arithmetic_trait_borrowed_to_owned,
-    arithmetic_trait_mixed_borrowed_owned,
+use crate::{
+    macros::arithmetics::{
+        arithmetic_between_types, arithmetic_trait_borrowed_to_owned,
+        arithmetic_trait_mixed_borrowed_owned,
+    },
+    rational::Q,
 };
-use flint_sys::fmpz::fmpz_add;
+use flint_sys::{fmpq::fmpq_add_fmpz, fmpz::fmpz_add};
 use std::ops::Add;
 
 impl Add for &Z {
@@ -50,6 +53,43 @@ impl Add for &Z {
 arithmetic_trait_borrowed_to_owned!(Add, add, Z, Z, Z);
 arithmetic_trait_mixed_borrowed_owned!(Add, add, Z, Z, Z);
 arithmetic_between_types!(Add, add, Z, Z, i64 i32 i16 i8 u64 u32 u16 u8);
+
+impl Add<&Q> for &Z {
+    type Output = Q;
+
+    /// Implements the [`Add`] trait for [`Z`] and [`Q`] values.
+    /// [`Add`] is implemented for any combination of owned and borrowed values.
+    ///
+    /// Parameters:
+    ///  - `other`: specifies the value to add to `self`
+    ///
+    /// Returns the sum of both numbers as a [`Q`].
+    ///
+    /// # Example
+    /// ```
+    /// use qfall_math::rational::Q;
+    /// use qfall_math::integer::Z;
+    /// use std::str::FromStr;
+    ///
+    /// let a: Z = Z::from_str("-42").unwrap();
+    /// let b: Q = Q::from_str("42/19").unwrap();
+    ///
+    /// let c: Q = &a + &b;
+    /// let d: Q = a + b;
+    /// let e: Q = &c + d;
+    /// let f: Q = c + &e;
+    /// ```
+    fn add(self, other: &Q) -> Self::Output {
+        let mut out = Q::default();
+        unsafe {
+            fmpq_add_fmpz(&mut out.value, &other.value, &self.value);
+        }
+        out
+    }
+}
+
+arithmetic_trait_borrowed_to_owned!(Add, add, Z, Q, Q);
+arithmetic_trait_mixed_borrowed_owned!(Add, add, Z, Q, Q);
 
 #[cfg(test)]
 mod test_add_between_types {
@@ -141,6 +181,70 @@ mod test_add_between_types {
         let _: Z = g + Z::from_str("42").unwrap();
         let _: Z = h + Z::from_str("42").unwrap();
         let _: Z = i + Z::from_str("42").unwrap();
+    }
+}
+
+#[cfg(test)]
+mod test_add_between_z_and_q {
+
+    use super::Z;
+    use crate::rational::Q;
+    use std::str::FromStr;
+
+    /// testing addition for [`Z`] and [`Q`]
+    #[test]
+    fn add() {
+        let a: Z = Z::from(4);
+        let b: Q = Q::from_str("5/7").unwrap();
+        let c: Q = a + b;
+        assert_eq!(c, Q::from_str("33/7").unwrap());
+    }
+
+    /// testing addition for both borrowed [`Z`] and [`Q`]
+    #[test]
+    fn add_borrow() {
+        let a: Z = Z::from(4);
+        let b: Q = Q::from_str("5/7").unwrap();
+        let c: Q = &a + &b;
+        assert_eq!(c, Q::from_str("33/7").unwrap());
+    }
+
+    /// testing addition for borrowed [`Z`] and [`Q`]
+    #[test]
+    fn add_first_borrowed() {
+        let a: Z = Z::from(4);
+        let b: Q = Q::from_str("5/7").unwrap();
+        let c: Q = &a + b;
+        assert_eq!(c, Q::from_str("33/7").unwrap());
+    }
+
+    /// testing addition for [`Z`] and borrowed [`Q`]
+    #[test]
+    fn add_second_borrowed() {
+        let a: Z = Z::from(4);
+        let b: Q = Q::from_str("5/7").unwrap();
+        let c: Q = a + &b;
+        assert_eq!(c, Q::from_str("33/7").unwrap());
+    }
+
+    /// testing addition for big numbers
+    #[test]
+    fn add_large_numbers() {
+        let a: Z = Z::from(u64::MAX);
+        let b: Q = Q::from_str(&format!("1/{}", u64::MAX)).unwrap();
+        let c: Q = Q::from_str(&format!("{}/2", u64::MAX)).unwrap();
+        let d: Q = &a + b;
+        let e: Q = a + c;
+        assert_eq!(
+            d,
+            Q::from_str(&format!("1/{}", u64::MAX)).unwrap()
+                + Q::from_str(&format!("{}/1", u64::MAX)).unwrap()
+        );
+        assert_eq!(
+            e,
+            Q::from_str(&format!("{}/1", u64::MAX)).unwrap()
+                + Q::from_str(&format!("{}/2", u64::MAX)).unwrap()
+        );
     }
 }
 
