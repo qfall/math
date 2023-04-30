43f338d808c10ede7f12d3d1de08b524e9294237
3pmile <pmile@mail.uni-paderborn.de>
2023-04-30 15:46:44 +0200

    mul Q between types

--- src/rational/q/arithmetic/mul.rs
+++ src/rational/q/arithmetic/mul.rs
@@ -9,10 +9,14 @@
 //! Implementation of the [`Mul`] trait for [`Q`] values.
 
 use super::super::Q;
-use crate::macros::arithmetics::{
-    arithmetic_trait_borrowed_to_owned, arithmetic_trait_mixed_borrowed_owned,
+use crate::{
+    integer::Z,
+    macros::arithmetics::{
+        arithmetic_between_types, arithmetic_trait_borrowed_to_owned,
+        arithmetic_trait_mixed_borrowed_owned,
+    },
 };
-use flint_sys::fmpq::fmpq_mul;
+use flint_sys::fmpq::{fmpq_mul, fmpq_mul_fmpz};
 use std::ops::Mul;
 
 impl Mul for &Q {
@@ -49,6 +53,44 @@ impl Mul for &Q {
 
 arithmetic_trait_borrowed_to_owned!(Mul, mul, Q, Q, Q);
 arithmetic_trait_mixed_borrowed_owned!(Mul, mul, Q, Q, Q);
+arithmetic_between_types!(Mul, mul, Q, Q, i64 i32 i16 i8 u64 u32 u16 u8 f32 f64);
+
+impl Mul<&Z> for &Q {
+    type Output = Q;
+
+    /// Implements the [`Mul`] trait for [`Q`] and [`Z`] values.
+    /// [`Mul`] is implemented for any combination of owned and borrowed values.
+    ///
+    /// Parameters:
+    ///  - `other`: specifies the value to multiply with `self`
+    ///
+    /// Returns the product of both numbers as a [`Q`].
+    ///
+    /// # Example
+    /// ```
+    /// use qfall_math::rational::Q;
+    /// use qfall_math::integer::Z;
+    /// use std::str::FromStr;
+    ///
+    /// let a: Q = Q::from_str("42/19").unwrap();
+    /// let b: Z = Z::from_str("-42").unwrap();
+    ///
+    /// let c: Q = &a * &b;
+    /// let d: Q = a * b;
+    /// let e: Q = &c * Z::from(42);
+    /// let f: Q = c * &Z::from(42);
+    /// ```
+    fn mul(self, other: &Z) -> Self::Output {
+        let mut out = Q::default();
+        unsafe {
+            fmpq_mul_fmpz(&mut out.value, &self.value, &other.value);
+        }
+        out
+    }
+}
+
+arithmetic_trait_borrowed_to_owned!(Mul, mul, Q, Z, Q);
+arithmetic_trait_mixed_borrowed_owned!(Mul, mul, Q, Z, Q);
 
 #[cfg(test)]
 mod test_mul {
@@ -109,5 +151,179 @@ mod test_mul {
             ))
             .unwrap()
         );
+    }
+}
+
+#[cfg(test)]
+mod test_mul_between_q_and_z {
+
+    use crate::integer::Z;
+    use crate::rational::Q;
+    use std::str::FromStr;
+
+    /// testing multiplication for [`Q`] and [`Z`]
+    #[test]
+    fn mul() {
+        let a: Q = Q::from_str("5/7").unwrap();
+        let b: Z = Z::from(4);
+        let c: Q = a * b;
+        assert_eq!(c, Q::from_str("20/7").unwrap());
+    }
+
+    /// testing multiplication for both borrowed [`Q`] and [`Z`]
+    #[test]
+    fn mul_borrow() {
+        let a: Q = Q::from_str("5/7").unwrap();
+        let b: Z = Z::from(4);
+        let c: Q = &a * &b;
+        assert_eq!(c, Q::from_str("20/7").unwrap());
+    }
+
+    /// testing multiplication for borrowed [`Q`] and [`Z`]
+    #[test]
+    fn mul_first_borrowed() {
+        let a: Q = Q::from_str("5/7").unwrap();
+        let b: Z = Z::from(4);
+        let c: Q = &a * b;
+        assert_eq!(c, Q::from_str("20/7").unwrap());
+    }
+
+    /// testing multiplication for [`Q`] and borrowed [`Z`]
+    #[test]
+    fn mul_second_borrowed() {
+        let a: Q = Q::from_str("5/7").unwrap();
+        let b: Z = Z::from(4);
+        let c: Q = a * &b;
+        assert_eq!(c, Q::from_str("20/7").unwrap());
+    }
+
+    /// testing multiplication for big numbers
+    #[test]
+    fn mul_large_numbers() {
+        let a: Q = Q::from_str(&format!("{}/2", u64::MAX)).unwrap();
+        let b: Q = Q::from_str(&format!("1/{}", u64::MAX)).unwrap();
+        let c: Z = Z::from(u64::MAX);
+        let d: Q = a * &c;
+        let e: Q = b * c;
+        assert_eq!(
+            d,
+            Q::from_str(&format!("{}/1", u64::MAX)).unwrap()
+                * Q::from_str(&format!("{}/2", u64::MAX)).unwrap()
+        );
+        assert_eq!(
+            e,
+            Q::from_str(&format!("1/{}", u64::MAX)).unwrap()
+                * Q::from_str(&format!("{}/1", u64::MAX)).unwrap()
+        );
+    }
+}
+
+#[cfg(test)]
+mod test_mul_between_types {
+
+    use crate::rational::Q;
+
+    /// testing multiplication between different types
+    #[test]
+    #[allow(clippy::op_ref)]
+    fn mul() {
+        let a: Q = Q::from(42);
+        let b: u64 = 1;
+        let c: u32 = 1;
+        let d: u16 = 1;
+        let e: u8 = 1;
+        let f: i64 = 1;
+        let g: i32 = 1;
+        let h: i16 = 1;
+        let i: i8 = 1;
+        let j: f32 = 0.3;
+        let k: f64 = 0.3;
+        let _: Q = &a * &b;
+        let _: Q = &a * &c;
+        let _: Q = &a * &d;
+        let _: Q = &a * &e;
+        let _: Q = &a * &f;
+        let _: Q = &a * &g;
+        let _: Q = &a * &h;
+        let _: Q = &a * &i;
+        let _: Q = &a * &j;
+        let _: Q = &a * &k;
+
+        let _: Q = &b * &a;
+        let _: Q = &c * &a;
+        let _: Q = &d * &a;
+        let _: Q = &e * &a;
+        let _: Q = &f * &a;
+        let _: Q = &g * &a;
+        let _: Q = &h * &a;
+        let _: Q = &i * &a;
+        let _: Q = &j * &a;
+        let _: Q = &k * &a;
+
+        let _: Q = &a * b;
+        let _: Q = &a * c;
+        let _: Q = &a * d;
+        let _: Q = &a * e;
+        let _: Q = &a * f;
+        let _: Q = &a * g;
+        let _: Q = &a * h;
+        let _: Q = &a * i;
+        let _: Q = &a * j;
+        let _: Q = &a * k;
+
+        let _: Q = &b * Q::from(42);
+        let _: Q = &c * Q::from(42);
+        let _: Q = &d * Q::from(42);
+        let _: Q = &e * Q::from(42);
+        let _: Q = &f * Q::from(42);
+        let _: Q = &g * Q::from(42);
+        let _: Q = &h * Q::from(42);
+        let _: Q = &i * Q::from(42);
+        let _: Q = &j * Q::from(42);
+        let _: Q = &k * Q::from(42);
+
+        let _: Q = Q::from(42) * &b;
+        let _: Q = Q::from(42) * &c;
+        let _: Q = Q::from(42) * &d;
+        let _: Q = Q::from(42) * &e;
+        let _: Q = Q::from(42) * &f;
+        let _: Q = Q::from(42) * &g;
+        let _: Q = Q::from(42) * &h;
+        let _: Q = Q::from(42) * &i;
+        let _: Q = Q::from(42) * &j;
+        let _: Q = Q::from(42) * &k;
+
+        let _: Q = b * &a;
+        let _: Q = c * &a;
+        let _: Q = d * &a;
+        let _: Q = e * &a;
+        let _: Q = f * &a;
+        let _: Q = g * &a;
+        let _: Q = h * &a;
+        let _: Q = i * &a;
+        let _: Q = j * &a;
+        let _: Q = k * &a;
+
+        let _: Q = Q::from(42) * b;
+        let _: Q = Q::from(42) * c;
+        let _: Q = Q::from(42) * d;
+        let _: Q = Q::from(42) * e;
+        let _: Q = Q::from(42) * f;
+        let _: Q = Q::from(42) * g;
+        let _: Q = Q::from(42) * h;
+        let _: Q = Q::from(42) * i;
+        let _: Q = Q::from(42) * j;
+        let _: Q = Q::from(42) * k;
+
+        let _: Q = b * Q::from(42);
+        let _: Q = c * Q::from(42);
+        let _: Q = d * Q::from(42);
+        let _: Q = e * Q::from(42);
+        let _: Q = f * Q::from(42);
+        let _: Q = g * Q::from(42);
+        let _: Q = h * Q::from(42);
+        let _: Q = i * Q::from(42);
+        let _: Q = j * Q::from(42);
+        let _: Q = k * Q::from(42);
     }
 }
