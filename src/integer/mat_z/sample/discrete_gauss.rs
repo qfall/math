00e0684d78aba66b151ed9b7bf9ad628e7490827
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-06-06 11:30:10 +0200

    Implement sample_discrete_gauss for MatZ

--- src/integer/mat_z/sample/discrete_gauss.rs
+++ src/integer/mat_z/sample/discrete_gauss.rs
@@ -12,10 +12,66 @@ use crate::{
     error::MathError,
     integer::{MatZ, Z},
     rational::{MatQ, Q},
+    traits::{GetNumColumns, GetNumRows, SetEntry},
     utils::sample::discrete_gauss::sample_d,
 };
+use std::fmt::Display;
 
 impl MatZ {
+    /// Initializes a new matrix with dimensions `num_rows` x `num_columns` and with each entry
+    /// sampled independently according to the discrete Gaussian distribution,
+    /// using [`Z::sample_discrete_gauss`].
+    ///
+    /// Parameters:
+    /// - `num_rows`: specifies the number of rows the new matrix should have
+    /// - `num_cols`: specifies the number of columns the new matrix should have
+    /// - `n`: specifies the range from which [`Z::sample_discrete_gauss`] samples
+    /// - `center`: specifies the positions of the center with peak probability
+    /// - `s`: specifies the Gaussian parameter, which is proportional
+    /// to the standard deviation `sigma * sqrt(2 * pi) = s`
+    ///
+    /// Returns a matrix with each entry sampled independently from the
+    /// specified discrete Gaussian distribution.
+    ///
+    /// # Example
+    /// ```
+    /// use qfall_math::integer::MatZ;
+    ///
+    /// let sample = MatZ::sample_discrete_gauss(3, 1, &1024, &0, &1.25f32).unwrap();
+    /// ```
+    ///
+    /// # Errors and Failures
+    /// - Returns a [`MathError`] of type
+    /// [`InvalidMatrix`](MathError::InvalidMatrix)
+    /// if the number of rows or columns is `0`.
+    /// - Returns a [`MathError`] of type [`OutOfBounds`](MathError::OutOfBounds)
+    /// if the number of rows or columns is negative or it does not fit into an [`i64`].
+    /// - Returns a [`MathError`] of type [`InvalidIntegerInput`](MathError::InvalidIntegerInput)
+    /// if the `n <= 1` or `s <= 0`.
+    pub fn sample_discrete_gauss<T1, T2, T3>(
+        num_rows: impl TryInto<i64> + Display,
+        num_cols: impl TryInto<i64> + Display,
+        n: &T1,
+        center: &T2,
+        s: &T3,
+    ) -> Result<MatZ, MathError>
+    where
+        T1: Into<Z> + Clone,
+        T2: Into<Q> + Clone,
+        T3: Into<Q> + Clone,
+    {
+        let mut out = Self::new(num_rows, num_cols)?;
+
+        for row in 0..out.get_num_rows() {
+            for col in 0..out.get_num_columns() {
+                let sample = Z::sample_discrete_gauss(n, center, s)?;
+                out.set_entry(row, col, sample)?;
+            }
+        }
+
+        Ok(out)
+    }
+
     /// SampleD samples a discrete Gaussian from the lattice with a provided `basis`.
     ///
     /// We do not check whether `basis` is actually a basis. Hence, the callee is
@@ -66,6 +122,40 @@ impl MatZ {
 
 #[cfg(test)]
 mod test_sample_discrete_gauss {
+    use crate::{
+        integer::{MatZ, Z},
+        rational::Q,
+    };
+
+    // This function only allows for a broader availability, which is tested here.
+
+    /// Checks whether `sample_discrete_gauss` is available for all types
+    /// implementing Into<Z> + Clone, i.e. u8, u16, u32, u64, i8, ...
+    /// or Into<Q> + Clone, i.e. u8, i16, f32, Z, Q, ...
+    #[test]
+    fn availability() {
+        let n = Z::from(1024);
+        let center = Q::from(0);
+        let s = Q::ONE;
+
+        let _ = MatZ::sample_discrete_gauss(2u64, 3i8, &16u16, &center, &1u16);
+        let _ = MatZ::sample_discrete_gauss(3u8, 2i16, &2u32, &center, &1u8);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2u64, &center, &1u32);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2i8, &center, &1u64);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2i16, &center, &1i64);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2i32, &center, &1i32);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2i64, &center, &1i16);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &n, &center, &1i8);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2u8, &center, &1i64);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2, &center, &n);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2, &center, &s);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2, &center, &1.25f64);
+        let _ = MatZ::sample_discrete_gauss(1, 1, &2, &center, &15.75f32);
+    }
+}
+
+#[cfg(test)]
+mod test_sample_d {
     use crate::{
         integer::{MatZ, Z},
         rational::{MatQ, Q},
