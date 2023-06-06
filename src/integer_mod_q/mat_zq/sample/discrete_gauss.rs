d85680372823a6357b301407069e61f657a5f4ae
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-06-06 16:40:18 +0200

    Apply suggestions from code review

--- src/integer_mod_q/mat_zq/sample/discrete_gauss.rs
+++ src/integer_mod_q/mat_zq/sample/discrete_gauss.rs
@@ -14,6 +14,7 @@ use crate::{
     integer_mod_q::MatZq,
     rational::Q,
     traits::{GetNumColumns, GetNumRows, SetEntry},
+    utils::sample::discrete_gauss::sample_z,
 };
 use std::fmt::Display;
 
@@ -37,7 +38,7 @@ impl MatZq {
     /// ```
     /// use qfall_math::integer_mod_q::MatZq;
     ///
-    /// let sample = MatZq::sample_discrete_gauss(3, 1, &83, &1024, &0, &1.25f32).unwrap();
+    /// let sample = MatZq::sample_discrete_gauss(3, 1, 83, 1024, 0, 1.25f32).unwrap();
     /// ```
     ///
     /// # Errors and Failures
@@ -51,23 +52,26 @@ impl MatZq {
     pub fn sample_discrete_gauss<T, T1, T2, T3>(
         num_rows: impl TryInto<i64> + Display,
         num_cols: impl TryInto<i64> + Display,
-        modulus: &T,
-        n: &T1,
-        center: &T2,
-        s: &T3,
+        modulus: T,
+        n: T1,
+        center: T2,
+        s: T3,
     ) -> Result<MatZq, MathError>
     where
-        T: Into<Z> + Clone,
-        T1: Into<Z> + Clone,
-        T2: Into<Q> + Clone,
-        T3: Into<Q> + Clone,
+        T: Into<Z>,
+        T1: Into<Z>,
+        T2: Into<Q>,
+        T3: Into<Q>,
     {
-        let modulus: Z = modulus.to_owned().into();
+        let modulus: Z = modulus.into();
+        let n: Z = n.into();
+        let center: Q = center.into();
+        let s: Q = s.into();
         let mut out = Self::new(num_rows, num_cols, modulus)?;
 
         for row in 0..out.get_num_rows() {
             for col in 0..out.get_num_columns() {
-                let sample = Z::sample_discrete_gauss(n, center, s)?;
+                let sample = sample_z(&n, &center, &s)?;
                 out.set_entry(row, col, sample).unwrap();
             }
         }
@@ -87,8 +91,8 @@ mod test_sample_discrete_gauss {
     // This function only allows for a broader availability, which is tested here.
 
     /// Checks whether `sample_discrete_gauss` is available for all types
-    /// implementing Into<Z> + Clone, i.e. u8, u16, u32, u64, i8, ...
-    /// or Into<Q> + Clone, i.e. u8, i16, f32, Z, Q, ...
+    /// implementing Into<Z>, i.e. u8, u16, u32, u64, i8, ...
+    /// or Into<Q>, i.e. u8, i16, f32, Z, Q, ...
     #[test]
     fn availability() {
         let n = Z::from(1024);
@@ -96,7 +100,7 @@ mod test_sample_discrete_gauss {
         let s = Q::ONE;
         let modulus = Modulus::try_from(&Z::from(83)).unwrap();
 
-        let _ = MatZq::sample_discrete_gauss(2u64, 3i8, &modulus, &16u16, &center, &1u16);
+        let _ = MatZq::sample_discrete_gauss(2u64, 3i8, &modulus, &16u16, 0f32, &1u16);
         let _ = MatZq::sample_discrete_gauss(3u8, 2i16, &83u8, &2u32, &center, &1u8);
         let _ = MatZq::sample_discrete_gauss(1, 1, &n, &2u64, &center, &1u32);
         let _ = MatZq::sample_discrete_gauss(1, 1, &83i8, &2i8, &center, &1u64);
@@ -107,7 +111,8 @@ mod test_sample_discrete_gauss {
         let _ = MatZq::sample_discrete_gauss(1, 1, &83, &2u8, &center, &1i64);
         let _ = MatZq::sample_discrete_gauss(1, 1, &83, &2, &center, &n);
         let _ = MatZq::sample_discrete_gauss(1, 1, &83, &2, &center, &s);
-        let _ = MatZq::sample_discrete_gauss(1, 1, &83, &2, &center, &1.25f64);
-        let _ = MatZq::sample_discrete_gauss(1, 1, &83, &2, &center, &15.75f32);
+        let _ = MatZq::sample_discrete_gauss(1, 1, &83, 2, &center, 1.25f64);
+        let _ = MatZq::sample_discrete_gauss(1, 1, &83, 2, 0, 1.25f64);
+        let _ = MatZq::sample_discrete_gauss(1, 1, 83, &2, center, 15.75f32);
     }
 }
