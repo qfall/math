fd282adb25f46ba8b7971fbdcaa762deb7e59776
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-06-13 08:56:18 +0200

    Implement sample_prime_uniform for Z

--- src/integer/z/sample/uniform.rs
+++ src/integer/z/sample/uniform.rs
@@ -39,17 +39,81 @@ impl Z {
     /// - Returns a [`MathError`] of type [`InvalidInterval`](MathError::InvalidInterval)
     /// if the given `upper_bound` isn't at least bigger than `lower_bound + 1`,
     /// i.e. the interval size is at most `1`.
-    pub fn sample_uniform<T1, T2>(lower_bound: &T1, upper_bound: &T2) -> Result<Self, MathError>
-    where
-        T1: Into<Z> + Clone,
-        T2: Into<Z> + Clone,
-    {
-        let lower_bound: Z = lower_bound.clone().into();
-        let upper_bound: Z = upper_bound.clone().into();
+    pub fn sample_uniform(
+        lower_bound: impl Into<Z>,
+        upper_bound: impl Into<Z>,
+    ) -> Result<Self, MathError> {
+        let lower_bound: Z = lower_bound.into();
+        let upper_bound: Z = upper_bound.into();
 
         let interval_size = &upper_bound - &lower_bound;
         let sample = sample_uniform_rejection(&interval_size)?;
         Ok(&lower_bound + sample)
+    }
+
+    /// Chooses a prime [`Z`] instance uniformly at random
+    /// in `[lower_bound, upper_bound)`. If after 2 * `n` steps, where `n` denotes the size of
+    /// the interval, no suitable prime was found, the algorithm aborts.
+    ///
+    /// The internally used uniform at random chosen bytes are generated
+    /// by [`ThreadRng`](rand::rngs::ThreadRng), which uses ChaCha12 and
+    /// is considered cryptographically secure.
+    ///
+    /// Parameters:
+    /// - `lower_bound`: specifies the included lower bound of the
+    /// interval over which is sampled
+    /// - `upper_bound`: specifies the excluded upper bound of the
+    /// interval over which is sampled
+    ///
+    /// Returns a fresh [`Z`] instance with a
+    /// uniform random value in `[lower_bound, upper_bound)`. Otherwise, a [`MathError`]
+    /// if the provided interval was chosen too small, no prime could be found in the interval,
+    /// or the provided `lower_bound` was negative.
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_math::integer::Z;
+    ///
+    /// let prime = Z::sample_prime_uniform(&1, &100).unwrap();
+    /// ```
+    ///
+    /// # Errors and Failures
+    /// - Returns a [`MathError`] of type [`InvalidInterval`](MathError::InvalidInterval)
+    /// if the given `upper_bound` isn't at least bigger than `lower_bound + 1`,
+    /// i.e. the interval size is at most `1`, or if no prime could be found in the specified interval.
+    /// - Returns a [`MathError`] of type [`InvalidIntegerInput`](MathError::InvalidIntegerInput)
+    /// if `lower_bound` is negative as primes are always positive.
+    pub fn sample_prime_uniform(
+        lower_bound: impl Into<Z>,
+        upper_bound: impl Into<Z>,
+    ) -> Result<Self, MathError> {
+        let lower_bound: Z = lower_bound.into();
+        let upper_bound: Z = upper_bound.into();
+
+        if lower_bound < Z::ZERO {
+            return Err(MathError::InvalidIntegerInput(format!(
+                "Primes are always positive. Hence, sample_prime_uniform expects a positive lower_bound.
+                The provided one is {lower_bound}.")));
+        }
+
+        let interval_size = &upper_bound - &lower_bound;
+        let mut sample = &lower_bound + sample_uniform_rejection(&interval_size)?;
+
+        // after 2 * size of interval many uniform random samples, a suitable prime should have been
+        // found with high probability, if there is one prime in the interval
+        let mut steps = 2 * &interval_size;
+        while !sample.is_prime() {
+            if steps == Z::ZERO {
+                return Err(MathError::InvalidInterval(format!(
+                    "After sampling O({interval_size}) times uniform at random in the interval [{lower_bound}, {upper_bound}[
+                        no prime was found. It is very likely, that no prime exists in this interval.
+                        Please choose the interval bigger.")));
+            }
+            sample = &lower_bound + sample_uniform_rejection(&interval_size)?;
+            steps = steps - 1;
+        }
+
+        Ok(sample)
     }
 }
 
@@ -100,7 +164,7 @@ mod test_sample_uniform {
     }
 
     /// Checks whether `sample_uniform` is available for all types
-    /// implementing Into<Z> + Clone, i.e. u8, u16, u32, u64, i8, ...
+    /// implementing Into<Z>, i.e. u8, u16, u32, u64, i8, ...
     #[test]
     fn availability() {
         let modulus = Modulus::from_str("7").unwrap();
@@ -140,5 +204,102 @@ mod test_sample_uniform {
             It fails if the sampled occurences do not look like a typical uniform random distribution. 
             If this happens, rerun the tests several times and check whether this issue comes up again.");
         }
+    }
+}
+
+#[cfg(test)]
+mod test_sample_prime_uniform {
+    use crate::{integer::Z, integer_mod_q::Modulus};
+    use std::str::FromStr;
+
+    /// Checks whether `sample_prime_uniform` outputs a prime sample every time.
+    #[test]
+    fn is_prime() {
+        for _ in 0..8 {
+            let sample = Z::sample_prime_uniform(20, i64::MAX).unwrap();
+            assert!(sample.is_prime());
+        }
+    }
+
+    /// Checks whether `sample_prime_uniform` returns an error if
+    /// no prime exists in the specified interval.
+    #[test]
+    fn no_prime_exists() {
+        let res_0 = Z::sample_prime_uniform(14, 17);
+        // there is no prime between u64::MAX - 57 and u64::MAX
+        let res_1 = Z::sample_prime_uniform(u64::MAX - 57, u64::MAX);
+
+        assert!(res_0.is_err());
+        assert!(res_1.is_err());
+    }
+
+    /// Checks whether a negative lower bound result in an error.
+    #[test]
+    fn negative_lower_bound() {
+        let res_0 = Z::sample_prime_uniform(-1, 10);
+        let res_1 = Z::sample_prime_uniform(-200, 1);
+
+        assert!(res_0.is_err());
+        assert!(res_1.is_err());
+    }
+
+    /// Checks whether the boundaries of the interval are kept for small intervals.
+    #[test]
+    fn boundaries_kept_small() {
+        let lower_bound = Z::from(17);
+        let upper_bound = Z::from(32);
+        for _ in 0..32 {
+            let sample = Z::sample_prime_uniform(&lower_bound, &upper_bound).unwrap();
+            assert!(lower_bound <= sample);
+            assert!(sample < upper_bound);
+        }
+    }
+
+    /// Checks whether the boundaries of the interval are kept for large intervals.
+    #[test]
+    fn boundaries_kept_large() {
+        let lower_bound = Z::ZERO;
+        let upper_bound = Z::from(i64::MAX);
+        for _ in 0..256 {
+            let sample = Z::sample_prime_uniform(&lower_bound, &upper_bound).unwrap();
+            assert!(lower_bound <= sample);
+            assert!(sample < upper_bound);
+        }
+    }
+
+    /// Checks whether providing an invalid interval results in an error.
+    #[test]
+    fn invalid_interval() {
+        let lb_0 = Z::from(i64::MIN) - Z::ONE;
+        let lb_1 = Z::from(i64::MIN);
+        let lb_2 = Z::ZERO;
+        let upper_bound = Z::from(i64::MIN);
+
+        let res_0 = Z::sample_prime_uniform(&lb_0, &upper_bound);
+        let res_1 = Z::sample_prime_uniform(&lb_1, &upper_bound);
+        let res_2 = Z::sample_prime_uniform(&lb_2, &upper_bound);
+
+        assert!(res_0.is_err());
+        assert!(res_1.is_err());
+        assert!(res_2.is_err());
+    }
+
+    /// Checks whether `sample_prime_uniform` is available for types
+    /// implementing Into<Z>, i.e. u8, u16, u32, u64, i8, ...
+    #[test]
+    fn availability() {
+        let modulus = Modulus::from_str("7").unwrap();
+        let z = Z::from(7);
+
+        let _ = Z::sample_prime_uniform(&0u16, &7u8);
+        let _ = Z::sample_prime_uniform(&0u32, &7u16);
+        let _ = Z::sample_prime_uniform(&0u64, &7u32);
+        let _ = Z::sample_prime_uniform(&0i8, &7u64);
+        let _ = Z::sample_prime_uniform(&0i16, &7i8);
+        let _ = Z::sample_prime_uniform(&0i32, &7i16);
+        let _ = Z::sample_prime_uniform(&0i64, &7i32);
+        let _ = Z::sample_prime_uniform(&Z::ZERO, &7i64);
+        let _ = Z::sample_prime_uniform(&0u8, &modulus);
+        let _ = Z::sample_prime_uniform(&0, &z);
     }
 }
