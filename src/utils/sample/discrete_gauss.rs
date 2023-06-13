0b5a925f842ad201dae9835e91321b20b26547a7
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-06-13 10:59:31 +0200

    Slight optimization of sample_d for common case

--- src/utils/sample/discrete_gauss.rs
+++ src/utils/sample/discrete_gauss.rs
@@ -185,7 +185,12 @@ pub(crate) fn sample_d(basis: &MatZ, n: &Z, center: &MatQ, s: &Q) -> Result<MatZ
     }
 
     // we know that norm_eucl_sqrd does not output errors for column vectors => we can unwrap
-    let basis = basis.sort_by_column(MatZ::norm_eucl_sqrd).unwrap();
+    // if basis is identity, there's no need to sort => better performance for common case
+    let basis = if basis.is_identity() {
+        basis.sort_by_column(MatZ::norm_eucl_sqrd).unwrap()
+    } else {
+        basis.to_owned()
+    };
     // we removed reversed ordering from the implementation of the challenge phase
     // as it does not seem to impact the length of the sampled vectors
     // and the paper states that the basis should be ordered, but not in which fashion.
