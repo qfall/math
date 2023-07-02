efcaaa42bcbee27653fb2d8b8fd58d1f4848f292
Sven Moog <smoog@mail.upb.de>
2023-07-02 23:40:30 +0200

    New exp algorithm for large numbers. - Taylor polynomial with the number of coefficients < x is very inaccurate for e^x. The error can quickly reach 10^300 and more. Additionally, the resulting Q has a large denominator which makes further calculations more expensive. - Now using a much better algorithm that is much faster and much more accurate than taylor. However, the accuracy bound is hard to determine. - Added test cases

--- src/rational/q/arithmetic/exp.rs
+++ src/rational/q/arithmetic/exp.rs
@@ -1,4 +1,4 @@
-// Copyright © 2023 Marvin Beckmann
+// Copyright © 2023 Marvin Beckmann and Sven Moog
 //
 // This file is part of qFALL-math.
 //
@@ -12,20 +12,17 @@ use crate::{
     rational::{PolyOverQ, Q},
     traits::Evaluate,
 };
-
-// Initialize the taylor expansion just once and not every time we call exp.
-lazy_static::lazy_static! {
-    /// The taylor expansion of the exponential function used when [`f64.exp`]
-    /// can no longer be used.
-    pub static ref EXP_TAYLOR_POLY: PolyOverQ = PolyOverQ::exp_function_taylor(100u32);
-}
+use flint_sys::fmpq::fmpq_mul_2exp;
 
 impl Q {
-    /// Computes `e^self`. This is done by first converting the value to a [`f64`],
-    /// evaluating the exponential function and converting the float back to [`Q`].
+    /// Computes `e^self`.
+    ///
+    /// For exponents below 709, the value is converted to [`f64`] for the calculation.
     /// As a result, the precision is limited to the precision of [`f64`].
-    /// More concrete, values smaller than `e^-745` are rounded to `0`.
-    /// For exponents above 709, a taylor series with 100 coefficients is used.
+    /// This means that values smaller than `e^-745` are rounded to `0`.
+    /// The [`f64`] calculation is relatively fast.
+    /// For exponents above 709, a different algorithm is used.
+    /// Its error bound is not calculated at this point, but probably below 10%.
     ///
     /// Returns e^self.
     ///
@@ -36,11 +33,28 @@ impl Q {
     /// let evaluation = Q::from((17, 3)).exp();
     /// let e = Q::ONE.exp();
     /// ```
+    ///
+    /// # Panics ...
+    /// - if the exponent is too large (exponent*log_2(e)) >= 2^36.
+    ///   This would use up more than 64 GB of RAM.
     pub fn exp(&self) -> Self {
-        if self < &Q::from(709) {
+        // f64::exp starts mapping to f64::INFINITY between 709.7 and 709.8.
+        if self <= &Q::from(709) {
             Q::from(f64::from(self).exp())
         } else {
-            EXP_TAYLOR_POLY.evaluate(self)
+            // e^x = 2^(log_2(e^x)) = 2^(x*log_2(e))
+            let log_2_of_e = Q::from(f64::log2(1f64.exp()));
+
+            let base_2_exponent = self * log_2_of_e;
+            let base_2_int_exponent: i64 = (&base_2_exponent.floor()).try_into().unwrap();
+            let base_2_remainder_exponent = base_2_exponent - base_2_int_exponent;
+
+            // Divide x*log_2(e) into an integer part i and the remainder r.
+            // 2^(i+r) = 2^i * 2^r
+            // 2^r ~= r+1 for r in [0,1]
+            let mut out: Q = base_2_remainder_exponent + 1;
+            unsafe { fmpq_mul_2exp(&mut out.value, &out.value, base_2_int_exponent as u64) }
+            out
         }
     }
 
@@ -69,6 +83,7 @@ impl Q {
 #[cfg(test)]
 mod test_exp {
     use super::*;
+    use flint_sys::fmpz::fmpz_get_d_2exp;
 
     /// Ensure that `e^0 = 1`.
     #[test]
@@ -81,7 +96,7 @@ mod test_exp {
     fn one() {
         let e = Q::ONE.exp();
 
-        assert_eq!(e, Q::from(1.0f64.exp()))
+        assert_eq!(e, Q::from(1f64.exp()))
     }
 
     /// Show the exp behavior for large negative inputs zero e^-745, but values smaller than e^-745 do.
@@ -100,20 +115,61 @@ mod test_exp {
 
     /// Test a large input very close to where exp stops using f64 for calculation.
     /// It should not saturate f64 into infinity.
+    /// Additionally, the the greater relationship should be correct when transitioning
+    /// to the other algorithm.
     #[test]
-    fn large_float_calc() {
-        let result = (Q::from(709) - Q::from((1, u64::MAX))).exp();
+    fn algorithm_transition_correct() {
+        let result_less_than_709 = (Q::from(709) - Q::from((1, u64::MAX))).exp();
 
-        assert_ne!(f64::from(&result), f64::INFINITY);
+        let result_709 = Q::from(709).exp();
+
+        assert_ne!(f64::from(&result_less_than_709), f64::INFINITY);
+        assert!(result_less_than_709 < result_709);
+    }
+
+    /// Ensure that e^300 is the same when calculated with [`Q`] or [`f64`].
+    #[test]
+    fn medium_exponent() {
+        let a = Q::from(300).exp();
+
+        assert_eq!(a, Q::from(300f64.exp()))
     }
 
     /// Ensure that the exponential function for large values outputs different results.
     #[test]
     fn large_exponent() {
-        let large_1 = Q::from(u64::MAX).exp();
-        let large_2 = Q::from(u64::MAX - 1).exp();
+        let large_1 = Q::from(u32::MAX).exp();
+        let large_2 = Q::from(u32::MAX - 1).exp();
 
         assert!(large_1 > large_2);
+    }
+
+    /// Ensure that the exponential function for [`u32::MAX`] has an
+    /// error of at most 6%.
+    #[test]
+    fn large_exponent_error_bound() {
+        let large = Q::from(u32::MAX).exp();
+
+        // large = e^u32::MAX ~= 1.490856880... × 10^1865280596
+        // <https://www.wolframalpha.com/input?i=1%2Be%5E4294967295>
+
+        // Calculations with these large numbers takes a lot of computing time.
+        // Therefore, we norm them with 2^-6196328016 for further calculations.
+        // 1.490856880 × 10^1865280596 * 2^-6196328016 = 2.42298514666...
+        // 10^1865280596 = 2^6196328016.7006445
+        // value_prime = large * 2^-6196328016
+        let mut exponent = 0;
+        let mut value_prime = unsafe { fmpz_get_d_2exp(&mut exponent, &large.value.num) };
+        value_prime = value_prime * 2f64.powi((exponent - 6196328016) as i32);
+
+        println!("{}, {}", value_prime, exponent);
+
+        // Allow 6% Error (Error Bound chosen based on calculation result)
+        let upper_bound = 1.06 * 2.42298514666;
+        let lower_bound = 0.94 * 2.42298514666;
+        // let upper_bound =
+        assert!(lower_bound < value_prime);
+        assert!(upper_bound > value_prime);
     }
 }
 
