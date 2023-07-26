252208f1b799697d3fa6345c4aee822077c111fb
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-07-26 14:46:50 +0200

    run cargo fmt

--- src/integer_mod_q/mat_polynomial_ring_zq/get.rs
+++ src/integer_mod_q/mat_polynomial_ring_zq/get.rs
@@ -41,7 +41,7 @@ impl MatPolynomialRingZq {
 impl MatPolynomialRingZq {
     /// Returns the [`MatPolyOverZ`] value of the [`MatPolynomialRingZq`] element.
     ///
-    /// The representation of each coefficient is returned. 
+    /// The representation of each coefficient is returned.
     /// It is in the range `[0,q[` (`0` inclusive, `q` exclusive).
     /// Each entry is reduced as much as possible.
     ///
