8ba51925777a588efe21896f7e5286f4212a77cc
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-07-26 13:28:41 +0200

    add get_window for MatPolyOverZ

--- src/integer/mat_poly_over_z/get.rs
+++ src/integer/mat_poly_over_z/get.rs
@@ -17,9 +17,12 @@ use crate::{
 };
 use flint_sys::{
     fmpz_poly::{fmpz_poly_set, fmpz_poly_struct},
-    fmpz_poly_mat::fmpz_poly_mat_entry,
+    fmpz_poly_mat::{
+        fmpz_poly_mat_entry, fmpz_poly_mat_init_set, fmpz_poly_mat_window_clear,
+        fmpz_poly_mat_window_init,
+    },
 };
-use std::fmt::Display;
+use std::{fmt::Display, mem::MaybeUninit};
 
 impl GetNumRows for MatPolyOverZ {
     /// Returns the number of rows of the matrix as a [`i64`].
@@ -130,16 +133,7 @@ impl MatPolyOverZ {
             ));
         }
 
-        let out = MatPolyOverZ::new(1, self.get_num_columns());
-        for column in 0..self.get_num_columns() {
-            unsafe {
-                fmpz_poly_set(
-                    fmpz_poly_mat_entry(&out.matrix, 0, column),
-                    fmpz_poly_mat_entry(&self.matrix, row_i64, column),
-                )
-            };
-        }
-        Ok(out)
+        self.get_submatrix(row_i64, row_i64, 0, self.get_num_columns() - 1)
     }
 
     /// Outputs a column vector of the specified column.
@@ -175,16 +169,78 @@ impl MatPolyOverZ {
             ));
         }
 
-        let out = MatPolyOverZ::new(self.get_num_rows(), 1);
-        for row in 0..self.get_num_rows() {
-            unsafe {
-                fmpz_poly_set(
-                    fmpz_poly_mat_entry(&out.matrix, row, 0),
-                    fmpz_poly_mat_entry(&self.matrix, row, column_i64),
-                )
-            };
+        self.get_submatrix(0, self.get_num_rows() - 1, column_i64, column_i64)
+    }
+
+    /// Returns a deep copy of the submatrix defined by the given parameters.
+    /// All entries starting from `(row1, col1)` to `(row2, col2)`(inclusively) are collected in
+    /// a new matrix.
+    /// Note that `row1 >= row2` and `col1 >= col2` must hold. Otherwise the function will panic.
+    ///
+    /// Parameters:
+    /// `row1`: The starting row of the submatrix
+    /// `row2`: The ending row of the submatrix
+    /// `col1`: The starting column of the submatrix
+    /// `col2`: The ending column of the submatrix
+    ///
+    /// Returns the submatrix from `(row1, col1)` to `(row2, col2)`(inclusively).
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_math::integer::MatPolyOverZ;
+    /// use std::str::FromStr;
+    ///
+    /// let mat = MatPolyOverZ::identity(3,3);
+    /// let sub_mat = mat.get_submatrix(0, 2, 1, 1).unwrap();
+    ///
+    /// let e2 = MatPolyOverZ::from_str("[[0],[1  1],[0]]").unwrap();
+    /// assert_eq!(e2, sub_mat)
+    /// ```
+    ///
+    /// # Errors and Failures
+    /// - Returns a [`MathError`] of type [`MathError::OutOfBounds`]
+    /// if any provided row or column is greater than the matrix or negative.
+    ///
+    /// # Panics ...
+    /// - if `col1 > col2` or `row1 > row2`.
+    pub fn get_submatrix(
+        &self,
+        row1: impl TryInto<i64> + Display,
+        row2: impl TryInto<i64> + Display,
+        col1: impl TryInto<i64> + Display,
+        col2: impl TryInto<i64> + Display,
+    ) -> Result<Self, MathError> {
+        let (row1, col1) = evaluate_indices_for_matrix(self, row1, col1)?;
+        let (row2, col2) = evaluate_indices_for_matrix(self, row2, col2)?;
+        assert!(
+            row2 >= row1,
+            "The number of rows must be positive, i.e. row2 ({row2}) must be greater or equal row1 ({row1})"
+        );
+
+        assert!(
+            col2 >= col1,
+            "The number of columns must be positive, i.e. col2 ({col2}) must be greater or equal col1 ({col1})"
+        );
+
+        // increase both values to have an inclusive capturing of the matrix entries
+        let (row2, col2) = (row2 + 1, col2 + 1);
+
+        let mut window = MaybeUninit::uninit();
+        // The memory for the elements of window is shared with self.
+        unsafe {
+            fmpz_poly_mat_window_init(window.as_mut_ptr(), &self.matrix, row1, col1, row2, col2)
+        };
+        let mut window_copy = MaybeUninit::uninit();
+        unsafe {
+            // Deep clone of the content of the window
+            fmpz_poly_mat_init_set(window_copy.as_mut_ptr(), window.as_ptr());
+            // Clears the matrix window and releases any memory that it uses. Note that
+            // the memory to the underlying matrix that window points to is not freed
+            fmpz_poly_mat_window_clear(window.as_mut_ptr());
         }
-        Ok(out)
+        Ok(MatPolyOverZ {
+            matrix: unsafe { window_copy.assume_init() },
+        })
     }
 
     #[allow(dead_code)]
@@ -422,6 +478,117 @@ mod test_get_vec {
         assert!(row2.is_err());
         assert!(column1.is_err());
         assert!(column2.is_err());
+    }
+}
+
+#[cfg(test)]
+mod test_get_submatrix {
+    use crate::{
+        integer::{MatPolyOverZ, Z},
+        traits::{GetNumColumns, GetNumRows},
+    };
+    use std::str::FromStr;
+
+    /// Ensures that getting the entire matrix as a submatrix works.
+    #[test]
+    fn entire_matrix() {
+        let mat = MatPolyOverZ::identity(5, 5);
+
+        let sub_mat = mat.get_submatrix(0, 4, 0, 4).unwrap();
+
+        assert_eq!(mat, sub_mat)
+    }
+
+    /// Ensures that a single matrix entry can be retrieved.
+    #[test]
+    fn matrix_single_entry() {
+        let mat = MatPolyOverZ::identity(5, 5);
+
+        let sub_mat = mat.get_submatrix(0, 0, 0, 0).unwrap();
+
+        let cmp_mat = MatPolyOverZ::identity(1, 1);
+        assert_eq!(cmp_mat, sub_mat)
+    }
+
+    /// Ensures that the dimensions of the submatrix are correct.
+    #[test]
+    fn correct_dimensions() {
+        let mat = MatPolyOverZ::identity(100, 100);
+
+        let sub_mat = mat.get_submatrix(1, 37, 0, 29).unwrap();
+
+        assert_eq!(37, sub_mat.get_num_rows());
+        assert_eq!(30, sub_mat.get_num_columns())
+    }
+
+    /// Ensures that a submatrix can be correctly retrieved for a matrix with large
+    /// entries.
+    #[test]
+    fn large_entries() {
+        let mat = MatPolyOverZ::from_str(&format!(
+            "[[2  {} {}, 1  2, 1  3],[1  1, 1  {}, 1  3]]",
+            u64::MAX,
+            i64::MAX,
+            i64::MIN
+        ))
+        .unwrap();
+
+        let sub_mat = mat.get_submatrix(0, 1, 0, 1).unwrap();
+
+        let cmp_mat = MatPolyOverZ::from_str(&format!(
+            "[[2  {} {}, 1  2],[1  1, 1  {}]]",
+            u64::MAX,
+            i64::MAX,
+            i64::MIN
+        ))
+        .unwrap();
+        assert_eq!(cmp_mat, sub_mat)
+    }
+
+    /// Ensures that an error is returned if coordinates are addressed that are not
+    /// within the matrix.
+    #[test]
+    fn invalid_coordinates() {
+        let mat = MatPolyOverZ::identity(10, 10);
+
+        assert!(mat.get_submatrix(0, 0, 0, 10).is_err());
+        assert!(mat.get_submatrix(0, 10, 0, 0).is_err());
+        assert!(mat.get_submatrix(0, 0, -1, 0).is_err());
+        assert!(mat.get_submatrix(-1, 0, 0, 0).is_err());
+    }
+
+    /// Ensures that the function panics if no columns of the matrix are addressed.
+    #[test]
+    #[should_panic]
+    fn no_columns() {
+        let mat = MatPolyOverZ::identity(10, 10);
+
+        let _ = mat.get_submatrix(0, 0, 6, 5);
+    }
+
+    /// Ensures that the function panics if no rows of the matrix are addressed.
+    #[test]
+    #[should_panic]
+    fn no_rows() {
+        let mat = MatPolyOverZ::identity(10, 10);
+
+        let _ = mat.get_submatrix(5, 4, 0, 0);
+    }
+
+    /// Ensure that the submatrix function can be called with several types.
+    #[test]
+    fn availability() {
+        let mat = MatPolyOverZ::identity(10, 10);
+
+        let _ = mat.get_submatrix(0_i8, 0_i8, 0_i8, 0_i8);
+        let _ = mat.get_submatrix(0_i16, 0_i16, 0_i16, 0_i16);
+        let _ = mat.get_submatrix(0_i32, 0_i32, 0_i32, 0_i32);
+        let _ = mat.get_submatrix(0_i64, 0_i64, 0_i64, 0_i64);
+        let _ = mat.get_submatrix(0_u8, 0_u8, 0_u8, 0_u8);
+        let _ = mat.get_submatrix(0_u16, 0_i16, 0_u16, 0_u16);
+        let _ = mat.get_submatrix(0_u32, 0_i32, 0_u32, 0_u32);
+        let _ = mat.get_submatrix(0_u64, 0_i64, 0_u64, 0_u64);
+        let _ = mat.get_submatrix(&Z::ZERO, &Z::ZERO, &Z::ZERO, &Z::ZERO);
     }
 }
 
