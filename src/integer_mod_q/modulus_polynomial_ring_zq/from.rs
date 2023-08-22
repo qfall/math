5aa9bf2d569fbc6f981007e3f58059e175330d5b
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-08-22 15:45:35 +0200

    Implement new_cyclic and new_anticyclic ModPolyRingZq

--- src/integer_mod_q/modulus_polynomial_ring_zq/from.rs
+++ src/integer_mod_q/modulus_polynomial_ring_zq/from.rs
@@ -13,9 +13,13 @@
 //! The explicit functions contain the documentation.
 
 use super::ModulusPolynomialRingZq;
-use crate::{error::MathError, integer_mod_q::PolyOverZq};
+use crate::{
+    error::MathError,
+    integer_mod_q::{Modulus, PolyOverZq},
+    traits::SetCoefficient,
+};
 use flint_sys::fq::fq_ctx_init_modulus;
-use std::{ffi::CString, mem::MaybeUninit, rc::Rc, str::FromStr};
+use std::{ffi::CString, fmt::Display, mem::MaybeUninit, rc::Rc, str::FromStr};
 
 impl From<&PolyOverZq> for ModulusPolynomialRingZq {
     /// Create a new Modulus object of type [`ModulusPolynomialRingZq`]
@@ -72,6 +76,70 @@ impl From<&ModulusPolynomialRingZq> for ModulusPolynomialRingZq {
     }
 }
 
+impl ModulusPolynomialRingZq {
+    /// Outputs a [`ModulusPolynomialRingZq`] of the form `X^n + 1 mod modulus`.
+    ///
+    /// Parameters:
+    /// - `n`: specifies the degree of the modulus polynomial
+    /// - `modulus`: specifies the modulus of the modulus polynomial
+    ///
+    /// Returns a [`ModulusPolynomialRingZq`] of the form `X^n + 1 mod modulus` or
+    /// a [`MathError`] if `modulus <= 1`, `n < 0`, or `n` does not into an [`i64`].
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_math::integer_mod_q::ModulusPolynomialRingZq;
+    ///
+    /// let poly_mod = ModulusPolynomialRingZq::new_anticyclic(8, 17);
+    /// ```
+    ///
+    /// # Errors and Failures
+    /// - Returns a [`MathError`] of type [`OutOfBounds`](MathError::OutOfBounds) if
+    /// the `n` is negative or it does not fit into an [`i64`].
+    ///
+    /// # Panics ...
+    /// - if the `modulus` is not larger than `1`.
+    pub fn new_anticyclic(
+        n: impl TryInto<i64> + Display,
+        modulus: impl Into<Modulus>,
+    ) -> Result<Self, MathError> {
+        let mut poly = PolyOverZq::from((1, modulus));
+        poly.set_coeff(n, 1)?;
+        Ok(ModulusPolynomialRingZq::from(&poly))
+    }
+
+    /// Outputs a [`ModulusPolynomialRingZq`] of the form `X^n - 1 mod modulus`.
+    ///
+    /// Parameters:
+    /// - `n`: specifies the degree of the modulus polynomial
+    /// - `modulus`: specifies the modulus of the modulus polynomial
+    ///
+    /// Returns a [`ModulusPolynomialRingZq`] of the form `X^n - 1 mod modulus` or
+    /// a [`MathError`] if `modulus <= 1`, `n < 0`, or `n` does not into an [`i64`].
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_math::integer_mod_q::ModulusPolynomialRingZq;
+    ///
+    /// let poly_mod = ModulusPolynomialRingZq::new_cyclic(8, 17);
+    /// ```
+    ///
+    /// # Errors and Failures
+    /// - Returns a [`MathError`] of type [`OutOfBounds`](MathError::OutOfBounds) if
+    /// the `n` is negative or it does not fit into an [`i64`].
+    ///
+    /// # Panics ...
+    /// - if the `modulus` is not larger than `1`.
+    pub fn new_cyclic(
+        n: impl TryInto<i64> + Display,
+        modulus: impl Into<Modulus>,
+    ) -> Result<Self, MathError> {
+        let mut poly = PolyOverZq::from((-1, modulus));
+        poly.set_coeff(n, 1)?;
+        Ok(ModulusPolynomialRingZq::from(&poly))
+    }
+}
+
 impl FromStr for ModulusPolynomialRingZq {
     type Err = MathError;
 
@@ -96,6 +164,126 @@ impl FromStr for ModulusPolynomialRingZq {
     /// - Returns a [`MathError`]. For further details see Errors and Failures of [`PolyOverZq::from_str`]
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(Self::from(&PolyOverZq::from_str(s)?))
+    }
+}
+
+#[cfg(test)]
+mod test_new_anticyclic {
+    use super::ModulusPolynomialRingZq;
+    use crate::{integer::Z, integer_mod_q::PolyOverZq, traits::GetCoefficient};
+
+    /// Ensure that the modulus polynomial has the specified degree.
+    #[test]
+    fn degree() {
+        let degrees = [1, 4, 7, 16, 32, 128];
+        for degree in degrees {
+            let poly_mod = ModulusPolynomialRingZq::new_anticyclic(degree, 7).unwrap();
+
+            assert_eq!(degree, poly_mod.get_degree());
+        }
+    }
+
+    /// Check whether the method outputs the correct polynomial.
+    #[test]
+    fn correct_polynomial() {
+        let degrees = [1, 4, 7, 16, 32, 128];
+        for degree in degrees {
+            let poly_mod = ModulusPolynomialRingZq::new_anticyclic(degree, 7).unwrap();
+            let poly_zq = PolyOverZq::from(&poly_mod);
+
+            assert_eq!(Z::ONE, poly_zq.get_coeff(degree).unwrap());
+            assert_eq!(Z::ONE, poly_zq.get_coeff(0).unwrap());
+            for i in 1..degree {
+                assert_eq!(Z::ZERO, poly_zq.get_coeff(i).unwrap());
+            }
+        }
+    }
+
+    /// Ensures that the correct modulus is set as
+    /// the integer modulus of the output modulus polynomial.
+    #[test]
+    fn correct_modulus() {
+        let moduli = [7, 10, i64::MAX];
+        for modulus in moduli {
+            let poly_mod = ModulusPolynomialRingZq::new_anticyclic(2, modulus).unwrap();
+
+            assert_eq!(Z::from(modulus), poly_mod.get_q());
+        }
+    }
+
+    /// Ensures that an invalid degree for the modulus polynomial results in an error.
+    #[test]
+    fn invalid_n() {
+        let res = ModulusPolynomialRingZq::new_anticyclic(-1, 7);
+
+        assert!(res.is_err());
+    }
+
+    /// Ensures that an invalid modulus for the modulus polynomial results in a panic.
+    #[test]
+    #[should_panic]
+    fn invalid_modulus() {
+        let _ = ModulusPolynomialRingZq::new_anticyclic(2, 0);
+    }
+}
+
+#[cfg(test)]
+mod test_new_cyclic {
+    use super::ModulusPolynomialRingZq;
+    use crate::{integer::Z, integer_mod_q::PolyOverZq, traits::GetCoefficient};
+
+    /// Ensure that the modulus polynomial has the specified degree.
+    #[test]
+    fn degree() {
+        let degrees = [1, 4, 7, 16, 32, 128];
+        for degree in degrees {
+            let poly_mod = ModulusPolynomialRingZq::new_cyclic(degree, 7).unwrap();
+
+            assert_eq!(degree, poly_mod.get_degree());
+        }
+    }
+
+    /// Check whether the method outputs the correct polynomial.
+    #[test]
+    fn correct_polynomial() {
+        let degrees = [1, 4, 7, 16, 32, 128];
+        for degree in degrees {
+            let poly_mod = ModulusPolynomialRingZq::new_cyclic(degree, 7).unwrap();
+            let poly_zq = PolyOverZq::from(&poly_mod);
+
+            assert_eq!(Z::ONE, poly_zq.get_coeff(degree).unwrap());
+            assert_eq!(Z::from(6), poly_zq.get_coeff(0).unwrap());
+            for i in 1..degree {
+                assert_eq!(Z::ZERO, poly_zq.get_coeff(i).unwrap());
+            }
+        }
+    }
+
+    /// Ensures that the correct modulus is set as
+    /// the integer modulus of the output modulus polynomial.
+    #[test]
+    fn correct_modulus() {
+        let moduli = [7, 10, i64::MAX];
+        for modulus in moduli {
+            let poly_mod = ModulusPolynomialRingZq::new_cyclic(2, modulus).unwrap();
+
+            assert_eq!(Z::from(modulus), poly_mod.get_q());
+        }
+    }
+
+    /// Ensures that an invalid degree for the modulus polynomial results in an error.
+    #[test]
+    fn invalid_n() {
+        let res = ModulusPolynomialRingZq::new_cyclic(-1, 7);
+
+        assert!(res.is_err());
+    }
+
+    /// Ensures that an invalid modulus for the modulus polynomial results in a panic.
+    #[test]
+    #[should_panic]
+    fn invalid_modulus() {
+        let _ = ModulusPolynomialRingZq::new_cyclic(2, 0);
     }
 }
 
