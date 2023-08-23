3ea267721634807a958690e1e29bfd228216f7d6
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-08-23 10:04:20 +0200

    Apply suggestions from code review

--- src/integer_mod_q/mat_zq/sample/binomial.rs
+++ src/integer_mod_q/mat_zq/sample/binomial.rs
@@ -34,7 +34,7 @@ impl MatZq {
     ///
     /// Returns a new [`MatZq`] instance with entries chosen
     /// according to the binomial distribution or a [`MathError`]
-    /// if `n < 1`, `p <= 0`, `p >= 1`, `n` does not fit into an [`i64`],
+    /// if `n < 1`, `p ∉ (0,1)`, `n` does not fit into an [`i64`],
     /// or the dimensions of the matrix were chosen too small.
     ///
     /// # Examples
@@ -48,7 +48,7 @@ impl MatZq {
     /// - Returns a [`MathError`] of type [`InvalidIntegerInput`](MathError::InvalidIntegerInput)
     /// if `n < 1`.
     /// - Returns a [`MathError`] of type [`InvalidInterval`](MathError::InvalidInterval)
-    /// if `p <= 0` or `p >= 1`.
+    /// if `p ∉ (0,1)`.
     /// - Returns a [`MathError`] of type [`ConversionError`](MathError::ConversionError)
     /// if `n` does not fit into an [`i64`].
     ///
