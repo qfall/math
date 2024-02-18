ce937b59d04bbccccf8a4236ad1dc019b5d479b5
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2024-02-18 21:20:23 +0100

    add a benchmark for solve

---
+++ benches/solve.rs
@@ -0,0 +1,41 @@
+// Copyright © 2023 Marvin Beckmann
+//
+// This file is part of qFALL-math.
+//
+// qFALL-math is free software: you can redistribute it and/or modify it under
+// the terms of the Mozilla Public License Version 2.0 as published by the
+// Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
+
+//! Create benchmark for [`MatZq::solve_gaussian_elimination`] in this file.
+
+use criterion::*;
+use qfall_math::integer_mod_q::MatZq;
+use std::{fs::File, io::Read};
+
+/// Benchmark [`MatZq::solve_gaussian_elimination`]
+///
+/// We uniformly sampled a matrix `A` and a vector `u`.
+/// Then we computed a target `v = A*u` and saved both as matrices.
+/// This benchmark loads both strings and runs the solve function to determine
+/// the time it takes to produce a solution.
+pub fn bench_solve(c: &mut Criterion) {
+    let mut matrix = String::new();
+    let mut f_matrix = File::open("benches/matrix.txt").expect("Unable to open file");
+    f_matrix
+        .read_to_string(&mut matrix)
+        .expect("Unable to read string");
+    let matrix: MatZq = serde_json::from_str(&matrix).unwrap();
+    let mut target = String::new();
+    let mut f_target = File::open("benches/target.txt").expect("Unable to open file");
+    f_target
+        .read_to_string(&mut target)
+        .expect("Unable to read string");
+
+    let target: MatZq = serde_json::from_str(&target).unwrap();
+
+    c.bench_function("Solve 100x200 mod 4096", |b| {
+        b.iter(|| matrix.solve_gaussian_elimination(&target))
+    });
+}
+
+criterion_group!(benches, bench_solve);
