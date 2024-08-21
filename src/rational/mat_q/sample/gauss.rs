2bf09d8452595ee34f5cfd81b3044f7acc04f19a
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2024-08-21 10:06:05 +0200

    modify function signature to be less error-prone

--- src/rational/mat_q/sample/gauss.rs
+++ src/rational/mat_q/sample/gauss.rs
@@ -11,18 +11,15 @@
 use crate::{
     error::MathError,
     rational::{MatQ, Q},
-    traits::{GetNumColumns, GetNumRows, SetEntry},
+    traits::{GetEntry, GetNumColumns, GetNumRows, SetEntry},
 };
-use std::fmt::Display;
 
 impl MatQ {
     /// Chooses a [`MatQ`] instance according to the continuous Gaussian distribution.
     /// Here, each entry is chosen according to the provided distribution.
     ///
     /// Parameters:
-    /// - `num_rows`: specifies the number of rows the new matrix should have
-    /// - `num_cols`: specifies the number of columns the new matrix should have
-    /// - `center`: specifies the position of the center
+    /// - `center`: specifies the center for each entry of the matrix
     /// - `sigma`: specifies the standard deviation
     ///
     /// Returns new [`MatQ`] sample chosen according to the specified continuous Gaussian
@@ -39,22 +36,14 @@ impl MatQ {
     /// # Errors and Failures
     /// - Returns a [`MathError`] of type [`NonPositive`](MathError::NonPositive)
     ///     if `sigma <= 0`.
-    ///
-    /// # Panics ...
-    /// - if the provided number of rows and columns are not suited to create a matrix.
-    ///     For further information see [`MatQ::new`].
-    pub fn sample_gauss(
-        num_rows: impl TryInto<i64> + Display,
-        num_cols: impl TryInto<i64> + Display,
-        center: impl Into<f64>,
-        sigma: impl Into<f64>,
-    ) -> Result<MatQ, MathError> {
-        let mut out = MatQ::new(num_rows, num_cols);
-        let (center, sigma) = (center.into(), sigma.into());
+    pub fn sample_gauss(center: &MatQ, sigma: impl Into<f64>) -> Result<MatQ, MathError> {
+        let mut out = MatQ::new(center.get_num_rows(), center.get_num_columns());
+        let sigma = sigma.into();
 
         for i in 0..out.get_num_rows() {
             for j in 0..out.get_num_columns() {
-                let sample = Q::sample_gauss(center, sigma)?;
+                let center_entry_ij = center.get_entry(i, j)?;
+                let sample = Q::sample_gauss(center_entry_ij, sigma)?;
                 out.set_entry(i, j, sample)?
             }
         }
@@ -65,43 +54,28 @@ impl MatQ {
 
 #[cfg(test)]
 mod test_sample_gauss {
-    use crate::rational::MatQ;
+    use crate::{
+        rational::MatQ,
+        traits::{GetNumColumns, GetNumRows},
+    };
 
     /// Ensure that an error is returned if `sigma` is not positive
     #[test]
     fn non_positive_sigma() {
-        let sample = MatQ::sample_gauss(5, 5, 0, 1).unwrap();
-        println!("{sample}");
-        for (mu, sigma) in [(0, 0), (0, -1)] {
-            assert!(MatQ::sample_gauss(2, 2, mu, sigma).is_err())
+        let center = MatQ::new(5, 5);
+        for sigma in [0, -1] {
+            assert!(MatQ::sample_gauss(&center, sigma).is_err())
         }
     }
 
-    /// Ensure that the function panics, if a negative number of rows is provided.
+    /// Ensure that the samples are of correct dimension
     #[test]
-    #[should_panic]
-    fn negative_rows() {
-        let _ = MatQ::sample_gauss(-1, 1, 0, 1);
-    }
-
-    /// Ensure that the function panics, if a negative number of columns is provided.
-    #[test]
-    #[should_panic]
-    fn negative_columns() {
-        let _ = MatQ::sample_gauss(1, -1, 0, 1);
-    }
-
-    /// Ensure that the function panics, if the number of columns is zero.
-    #[test]
-    #[should_panic]
-    fn zero_columns() {
-        let _ = MatQ::sample_gauss(1, 0, 0, 1);
-    }
-
-    /// Ensure that the function panics, if the number of rows is zero.
-    #[test]
-    #[should_panic]
-    fn zero_rows() {
-        let _ = MatQ::sample_gauss(0, 1, 0, 1);
+    fn correct_dimension() {
+        for (x, y) in [(5, 5), (1, 10), (10, 1)] {
+            let center = MatQ::new(x, y);
+            let sample = MatQ::sample_gauss(&center, 1).unwrap();
+            assert_eq!(center.get_num_rows(), sample.get_num_rows());
+            assert_eq!(center.get_num_columns(), sample.get_num_columns());
+        }
     }
 }
