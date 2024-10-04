fa9a3c0fba33cea7aee94130b7d62b14901cd341
Marcel Schmidt <marcel.schmidtfuldatal@gmail.com>
2024-10-04 15:09:11 +0200

    Fixed punctuation

--- src/integer_mod_q/polynomial_ring_zq/get.rs
+++ src/integer_mod_q/polynomial_ring_zq/get.rs
@@ -66,8 +66,8 @@ impl GetCoefficient<Z> for PolynomialRingZq {
     /// Parameters:
     /// - `index`: the index of the coefficient to get (has to be positive)
     ///
-    /// Returns the coefficient as a [`Z`] or a [`MathError`] if the provided index
-    /// is negative and therefore invalid or it does not fit into an [`i64`].
+    /// Returns the coefficient as a [`Z`], or a [`MathError`] if the provided index
+    /// is negative and therefore invalid, or it does not fit into an [`i64`].
     ///
     /// # Examples
     /// ```
