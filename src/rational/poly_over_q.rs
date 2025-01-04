2bdfc662d8c4813562e25cf6e5fdba97509b954f
Marcel Schmidt <marcel.schmidtfuldatal@gmail.com>
2025-01-04 14:09:57 +0100

    Implemented sample_gauss for PolyOverQ

--- src/rational/poly_over_q.rs
+++ src/rational/poly_over_q.rs
@@ -28,6 +28,7 @@ mod get;
 mod norm;
 mod ownership;
 mod properties;
+mod sample;
 mod serialize;
 mod set;
 mod to_string;
---
+++ src/rational/poly_over_q/sample.rs
@@ -0,0 +1,11 @@
+// Copyright © 2024 Marcel Luca Schmidt
+//
+// This file is part of qFALL-math.
+//
+// qFALL-math is free software: you can redistribute it and/or modify it under
+// the terms of the Mozilla Public License Version 2.0 as published by the
+// Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
+
+//! This module contains sampling algorithms for different distributions.
+
+mod gauss;
