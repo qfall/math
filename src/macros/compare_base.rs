5edb4913ec36ca287b63a1691aa003c7c47639da
Marvin Beckmann <mabeck@dtu.dk>
2025-04-20 12:35:12 +0200

    add doc comments for compare base macros

--- src/macros/compare_base.rs
+++ src/macros/compare_base.rs
@@ -1,3 +1,28 @@
+// Copyright © 2025 Marvin Beckmann
+//
+// This file is part of qFALL-math.
+//
+// qFALL-math is free software: you can redistribute it and/or modify it under
+// the terms of the Mozilla Public License Version 2.0 as published by the
+// Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
+
+//! This module implements macros which are used to implement the compare base trait for data types.
+
+/// Implements the `CompareBase` trait for `*type*` and `&*type*`.
+///
+/// Parameters:
+/// - `type`: the type the trait is implemented for
+///   (e.g. [`Zq`](crate::integer_mod_q::Zq)
+/// - `source_type`: tthe type for which the basic comparison is implemented
+/// - `get_mod_expr`: the expression that returns the modulus of the type for self
+///
+/// Returns the trait implementations for the provided types just comparing the modulus
+/// of both provided inputs.
+///
+/// # Example
+/// ```compile_fail
+/// compare_base_impl!(Zq, Zq, |s: &Zq| s.get_mod());
+/// ```
 macro_rules! compare_base_impl {
     ($type:ident, $source_type:ty, $get_mod_expr:expr) => {
         impl CompareBase<$source_type> for $type {
@@ -62,6 +87,20 @@ macro_rules! compare_base_impl {
     };
 }
 
+/// Implements the `CompareBase` trait for `*type*`, using modulo comparison.
+///
+/// Parameters:
+/// - `type`: the type the trait is implemented for
+///   (e.g. [`Zq`](crate::integer_mod_q::Zq)
+/// - `source_type`: a list of types for which the basic comparison is implemented
+///
+/// Returns the trait implementations for the provided types just comparing the modulus
+/// of both provided inputs
+///
+/// # Example
+/// ```compile_fail
+/// compare_base_get_mod!(Zq for Zq);
+/// ```
 macro_rules! compare_base_get_mod {
     ($type:ident for $($source_type:ident)*) => {
         $(
@@ -70,6 +109,22 @@ macro_rules! compare_base_get_mod {
     };
 }
 
+/// Implements the `CompareBase` trait for `*type*`, using modulo comparison.
+/// Here, self and other have different moduli types, and for self, we specifically
+/// return the integer modulus possibly contained in polynomial moduli.
+///
+/// Parameters:
+/// - `type`: the type the trait is implemented for
+///   (e.g. [`PolynomialRingZq`](crate::integer_mod_q::PolynomialRingZq)
+/// - `source_type`: a list of types for which the basic comparison is implemented
+///
+/// Returns the trait implementations for the provided types just comparing the modulus
+/// of both provided inputs
+///
+/// # Example
+/// ```compile_fail
+/// compare_base_get_mod_get_q!(PolynomialRingZq for Zq);
+/// ```
 macro_rules! compare_base_get_mod_get_q {
     ($type:ident for $($source_type:ident)*) => {
         $(
@@ -78,6 +133,20 @@ macro_rules! compare_base_get_mod_get_q {
     };
 }
 
+/// Implements the default implementation of the `CompareBase` trait, just returning `true`
+/// and no error.
+///
+/// Parameters:
+/// - `type`: the type the trait is implemented for
+///   (e.g. [`PolynomialRingZq`](crate::integer_mod_q::PolynomialRingZq)
+/// - `source_type`: a list of types for which the basic comparison is implemented
+///
+/// Returns the default trait implementations for the provided types.
+///
+/// # Example
+/// ```compile_fail
+/// compare_base_get_mod_get_q!(Zq for Z);
+/// ```
 macro_rules! compare_base_default {
     ($type:ident for $($source_type:ident)*) => {
         $(
