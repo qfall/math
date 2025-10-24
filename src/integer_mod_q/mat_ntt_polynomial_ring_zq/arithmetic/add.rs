a308ba9abdf04e93a3253e382f0b6487ef1fe47a
jnsiemer <niklas.siemer@gmail.com>
2025-10-24 21:20:48 +0100

    Add arithmetic of MatNTT

---
+++ src/integer_mod_q/mat_ntt_polynomial_ring_zq/arithmetic/add.rs
@@ -0,0 +1,156 @@
+// Copyright © 2025 Niklas Siemer
+//
+// This file is part of qFALL-math.
+//
+// qFALL-math is free software: you can redistribute it and/or modify it under
+// the terms of the Mozilla Public License Version 2.0 as published by the
+// Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
+
+//! Implementation of addition for [`MatNTTPolynomialRingZq`].
+
+use crate::integer_mod_q::{MatNTTPolynomialRingZq, Modulus};
+
+impl MatNTTPolynomialRingZq {
+    /// Adds `self` with `other` using component-wise (NTT) addition.
+    ///
+    /// Paramters:
+    /// - `other`: specifies the NTT-representation of the polynomial to add to `self`
+    /// - `modulus`: defines the modulus `q`
+    ///
+    /// Returns the NTT-representation of the sum of `self` and `other` generated
+    /// with respect to `modulus`.
+    ///
+    /// # Example
+    /// ```
+    /// use qfall_math::integer_mod_q::{MatNTTPolynomialRingZq, Modulus};
+    /// use crate::qfall_math::traits::SetCoefficient;
+    ///
+    /// let n = 4;
+    /// let q = Modulus::from(257);
+    ///
+    /// let a = MatNTTPolynomialRingZq::sample_uniform(2, 3, n, &q);
+    /// let b = MatNTTPolynomialRingZq::sample_uniform(3, 4, n, &q);
+    ///
+    /// a.add_assign(&b, &q);
+    /// ```
+    ///
+    /// # Panics ...
+    /// - if the `modulus` is smaller than `2`.
+    /// - if the number of rows of `self` and `other` or their number of columns is not equal.
+    pub fn add_assign(&mut self, other: &Self, modulus: &Modulus) {
+        assert_eq!(
+            self.get_num_rows(),
+            other.get_num_rows(),
+            "The number of rows of `self` and `other` has to be equal for matrix addition."
+        );
+        assert_eq!(
+            self.get_num_columns(),
+            other.get_num_columns(),
+            "The number of columns of `self` and `other` has to be equal for matrix addition."
+        );
+
+        for col in 0..other.get_num_columns() {
+            for row in 0..self.get_num_rows() {
+                self.matrix[col][row].add_assign(&other.matrix[col][row], modulus);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_add {
+    use crate::{
+        integer_mod_q::{
+            MatNTTPolynomialRingZq, MatPolynomialRingZq, Modulus, ModulusPolynomialRingZq,
+            PolyOverZq,
+        },
+        traits::SetCoefficient,
+    };
+
+    /// Ensure that the entrywise addition and the intuitive addition yields
+    /// the same results for small parameters.
+    #[test]
+    fn test_small_params() {
+        let n = 4;
+        let modulus = 257;
+
+        let mut mod_poly = PolyOverZq::from(modulus);
+        mod_poly.set_coeff(0, 1).unwrap();
+        mod_poly.set_coeff(n, 1).unwrap();
+
+        let mut polynomial_modulus = ModulusPolynomialRingZq::from(&mod_poly);
+
+        polynomial_modulus.set_ntt_unchecked(64);
+
+        let p1 = MatPolynomialRingZq::sample_uniform(3, 4, &polynomial_modulus);
+        let p2 = MatPolynomialRingZq::sample_uniform(3, 4, &polynomial_modulus);
+
+        let mut ntt1 = MatNTTPolynomialRingZq::from(&p1);
+        let ntt2 = MatNTTPolynomialRingZq::from(&p2);
+
+        ntt1.add_assign(&ntt2, &Modulus::from(modulus));
+
+        assert_eq!(
+            &p1 + &p2,
+            MatPolynomialRingZq::from((ntt1, &polynomial_modulus))
+        )
+    }
+
+    /// Ensure that the entrywise addition and the intuitive addition yields
+    /// the same results for the parameters from Dilithium.
+    #[test]
+    fn test_dilithium_params() {
+        let n = 256;
+        let modulus = 2_i64.pow(23) - 2_i64.pow(13) + 1;
+
+        let mut mod_poly = PolyOverZq::from(modulus);
+        mod_poly.set_coeff(0, 1).unwrap();
+        mod_poly.set_coeff(n, 1).unwrap();
+
+        let mut polynomial_modulus = ModulusPolynomialRingZq::from(&mod_poly);
+
+        polynomial_modulus.set_ntt_unchecked(1753);
+
+        let p1 = MatPolynomialRingZq::sample_uniform(4, 2, &polynomial_modulus);
+        let p2 = MatPolynomialRingZq::sample_uniform(4, 2, &polynomial_modulus);
+
+        let mut ntt1 = MatNTTPolynomialRingZq::from(&p1);
+        let ntt2 = MatNTTPolynomialRingZq::from(&p2);
+
+        ntt1.add_assign(&ntt2, &Modulus::from(modulus));
+
+        assert_eq!(
+            &p1 + &p2,
+            MatPolynomialRingZq::from((ntt1, &polynomial_modulus))
+        )
+    }
+
+    /// Ensure that the entrywise addition and the intuitive addition yields
+    /// the same results for the parameters from Hawk1024.
+    #[test]
+    fn test_hawk1024_params() {
+        let n = 1024;
+        let modulus = 12289;
+
+        let mut mod_poly = PolyOverZq::from(modulus);
+        mod_poly.set_coeff(0, 1).unwrap();
+        mod_poly.set_coeff(n, 1).unwrap();
+
+        let mut polynomial_modulus = ModulusPolynomialRingZq::from(&mod_poly);
+
+        polynomial_modulus.set_ntt_unchecked(1945);
+
+        let p1 = MatPolynomialRingZq::sample_uniform(2, 2, &polynomial_modulus);
+        let p2 = MatPolynomialRingZq::sample_uniform(2, 2, &polynomial_modulus);
+
+        let mut ntt1 = MatNTTPolynomialRingZq::from(&p1);
+        let ntt2 = MatNTTPolynomialRingZq::from(&p2);
+
+        ntt1.add_assign(&ntt2, &Modulus::from(modulus));
+
+        assert_eq!(
+            &p1 + &p2,
+            MatPolynomialRingZq::from((ntt1, &polynomial_modulus))
+        )
+    }
+}
---
+++ src/integer_mod_q/mat_ntt_polynomial_ring_zq/arithmetic/mul.rs
@@ -0,0 +1,161 @@
+// Copyright © 2025 Niklas Siemer
+//
+// This file is part of qFALL-math.
+//
+// qFALL-math is free software: you can redistribute it and/or modify it under
+// the terms of the Mozilla Public License Version 2.0 as published by the
+// Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
+
+//! Implementation of multipliation for [`MatNTTPolynomialRingZq`].
+
+use crate::integer_mod_q::{MatNTTPolynomialRingZq, Modulus};
+
+impl MatNTTPolynomialRingZq {
+    /// Multiplies `self` with `other` using component-wise (NTT) multiplication.
+    ///
+    /// Paramters:
+    /// - `other`: specifies the NTT-representation of the polynomial to multiply to `self`
+    /// - `modulus`: defines the modulus `q`
+    ///
+    /// Returns the NTT-representation of the multiplication of `self` and `other` generated
+    /// with respect to `modulus`.
+    ///
+    /// # Example
+    /// ```
+    /// use qfall_math::integer_mod_q::{MatNTTPolynomialRingZq, Modulus};
+    /// use crate::qfall_math::traits::SetCoefficient;
+    ///
+    /// let n = 4;
+    /// let q = Modulus::from(257);
+    ///
+    /// let a = MatNTTPolynomialRingZq::sample_uniform(2, 3, n, &q);
+    /// let b = MatNTTPolynomialRingZq::sample_uniform(3, 4, n, &q);
+    ///
+    /// a.mul(&b, &q);
+    /// ```
+    ///
+    /// # Panics ...
+    /// - if the `modulus` is smaller than `2`.
+    /// - if the number of rows of `self` and the number of columns of `other` does not match up.
+    pub fn mul(&self, other: &Self, modulus: &Modulus) -> Self {
+        assert_eq!(self.get_num_columns(), other.get_num_rows(),
+            "The number of rows of `self` and the number of columns of `other` has to be equal for matrix multiplication.");
+
+        let mut res = Vec::with_capacity(other.get_num_columns());
+
+        for col in 0..other.get_num_columns() {
+            let mut col_vec = Vec::with_capacity(self.get_num_rows());
+            for row in 0..self.get_num_rows() {
+                let mut entry = self.matrix[0][row].mul(&other.matrix[col][0], modulus);
+                for i in 1..self.get_num_columns() {
+                    entry.add_assign(
+                        &self.matrix[i][row].mul(&other.matrix[col][i], modulus),
+                        modulus,
+                    );
+                }
+                col_vec.push(entry);
+            }
+            res.push(col_vec);
+        }
+
+        MatNTTPolynomialRingZq { matrix: res }
+    }
+}
+
+#[cfg(test)]
+mod test_mul {
+    use crate::{
+        integer_mod_q::{
+            MatNTTPolynomialRingZq, MatPolynomialRingZq, Modulus, ModulusPolynomialRingZq,
+            PolyOverZq,
+        },
+        traits::SetCoefficient,
+    };
+
+    /// Ensure that the entrywise multiplication and the intuitive multiplication yields
+    /// the same results for small parameters.
+    #[test]
+    fn test_small_params() {
+        let n = 4;
+        let modulus = 257;
+
+        let mut mod_poly = PolyOverZq::from(modulus);
+        mod_poly.set_coeff(0, 1).unwrap();
+        mod_poly.set_coeff(n, 1).unwrap();
+
+        let mut polynomial_modulus = ModulusPolynomialRingZq::from(&mod_poly);
+
+        polynomial_modulus.set_ntt_unchecked(64);
+
+        let p1 = MatPolynomialRingZq::sample_uniform(3, 3, &polynomial_modulus);
+        let p2 = MatPolynomialRingZq::sample_uniform(3, 4, &polynomial_modulus);
+
+        let ntt1 = MatNTTPolynomialRingZq::from(&p1);
+        let ntt2 = MatNTTPolynomialRingZq::from(&p2);
+
+        let ntt_res = ntt1.mul(&ntt2, &Modulus::from(modulus));
+
+        assert_eq!(
+            &p1 * &p2,
+            MatPolynomialRingZq::from((ntt_res, &polynomial_modulus))
+        )
+    }
+
+    /// Ensure that the entrywise multiplication and the intuitive multiplication yields
+    /// the same results for the parameters from Dilithium.
+    #[test]
+    fn test_dilithium_params() {
+        let n = 256;
+        let modulus = 2_i64.pow(23) - 2_i64.pow(13) + 1;
+
+        let mut mod_poly = PolyOverZq::from(modulus);
+        mod_poly.set_coeff(0, 1).unwrap();
+        mod_poly.set_coeff(n, 1).unwrap();
+
+        let mut polynomial_modulus = ModulusPolynomialRingZq::from(&mod_poly);
+
+        polynomial_modulus.set_ntt_unchecked(1753);
+
+        let p1 = MatPolynomialRingZq::sample_uniform(2, 3, &polynomial_modulus);
+        let p2 = MatPolynomialRingZq::sample_uniform(3, 4, &polynomial_modulus);
+
+        let ntt1 = MatNTTPolynomialRingZq::from(&p1);
+        let ntt2 = MatNTTPolynomialRingZq::from(&p2);
+
+        let ntt_res = ntt1.mul(&ntt2, &Modulus::from(modulus));
+
+        assert_eq!(
+            &p1 * &p2,
+            MatPolynomialRingZq::from((ntt_res, &polynomial_modulus))
+        )
+    }
+
+    /// Ensure that the entrywise multiplication and the intuitive multiplication yields
+    /// the same results for the parameters from Hawk1024.
+    #[test]
+    fn test_hawk1024_params() {
+        let n = 1024;
+        let modulus = 12289;
+
+        let mut mod_poly = PolyOverZq::from(modulus);
+        mod_poly.set_coeff(0, 1).unwrap();
+        mod_poly.set_coeff(n, 1).unwrap();
+
+        let mut polynomial_modulus = ModulusPolynomialRingZq::from(&mod_poly);
+
+        polynomial_modulus.set_ntt_unchecked(1945);
+
+        let p1 = MatPolynomialRingZq::sample_uniform(2, 3, &polynomial_modulus);
+        let p2 = MatPolynomialRingZq::sample_uniform(3, 4, &polynomial_modulus);
+
+        let ntt1 = MatNTTPolynomialRingZq::from(&p1);
+        let ntt2 = MatNTTPolynomialRingZq::from(&p2);
+
+        let ntt_res = ntt1.mul(&ntt2, &Modulus::from(modulus));
+
+        assert_eq!(
+            &p1 * &p2,
+            MatPolynomialRingZq::from((ntt_res, &polynomial_modulus))
+        )
+    }
+}
