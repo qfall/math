095ad48e250d3bf8288f1729fbe51d6352fc1cd8
jnsiemer <niklas.siemer@gmail.com>
2025-10-24 21:20:33 +0100

    Add basic getters

---
+++ src/integer_mod_q/mat_ntt_polynomial_ring_zq/get.rs
@@ -0,0 +1,41 @@
+// Copyright © 2025 Niklas Siemer
+//
+// This file is part of qFALL-math.
+//
+// qFALL-math is free software: you can redistribute it and/or modify it under
+// the terms of the Mozilla Public License Version 2.0 as published by the
+// Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
+
+//! Implementations to get information about a [`MatNTTPolynomialRingZq`] matrix.
+
+use crate::integer_mod_q::MatNTTPolynomialRingZq;
+
+impl MatNTTPolynomialRingZq {
+    /// Returns the number of rows of the matrix as a [`usize`].
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_math::integer_mod_q::MatZq;
+    /// use qfall_math::traits::*;
+    ///
+    /// let matrix = MatZq::new(5, 6, 7);
+    /// let rows = matrix.get_num_rows();
+    /// ```
+    pub fn get_num_rows(&self) -> usize {
+        self.matrix[0].len()
+    }
+
+    /// Returns the number of columns of the matrix as a [`usize`].
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_math::integer_mod_q::MatZq;
+    /// use qfall_math::traits::*;
+    ///
+    /// let matrix = MatZq::new(5, 6, 7);
+    /// let rows = matrix.get_num_columns();
+    /// ```
+    pub fn get_num_columns(&self) -> usize {
+        self.matrix.len()
+    }
+}
