e21a4cd0d9f0ad9ba8e72e52782a06e2b60ca041
jnsiemer <niklas.siemer@gmail.com>
2026-04-28 00:46:52 +0100

    Add mod_pm for integer datatypes

--- src/integer/poly_over_z/arithmetic/modulo.rs
+++ src/integer/poly_over_z/arithmetic/modulo.rs
@@ -16,6 +16,7 @@ use crate::{
         arithmetics::{arithmetic_trait_borrowed_to_owned, arithmetic_trait_mixed_borrowed_owned},
         for_others::implement_for_others,
     },
+    traits::{GetCoefficient, SetCoefficient},
 };
 use flint_sys::fmpz_poly::fmpz_poly_scalar_mod_fmpz;
 use std::ops::Rem;
@@ -141,6 +142,47 @@ arithmetic_trait_borrowed_to_owned!(Rem, rem, PolyOverZ, Z, PolyOverZ);
 arithmetic_trait_mixed_borrowed_owned!(Rem, rem, PolyOverZ, Z, PolyOverZ);
 
 implement_for_others!(Z, PolyOverZ, Rem for i8 i16 i32 i64 u8 u16 u32 u64);
+
+impl PolyOverZ {
+    /// Computes `self` mod± `modulus` as long as `modulus` is greater than 1, i.e.
+    /// it returns a polynomial with coefficients in `[-⌈q/2⌉, ⌈q/2⌉]`.
+    ///
+    /// Parameters:
+    /// - `modulus`: specifies a non-zero integer
+    ///   over which the remainder closest to `0` is computed
+    ///
+    /// Returns `self` mod± `modulus` as a [`PolyOverZ`] instance.
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_math::integer::PolyOverZ;
+    ///
+    /// let a = PolyOverZ::from(42);
+    ///
+    /// let c = a.mod_pm(24);
+    ///
+    /// assert_eq!(PolyOverZ::from(-6), c);
+    /// ```
+    ///
+    /// # Panics ...
+    /// - if `modulus` is smaller than `2`.
+    pub fn mod_pm(&self, modulus: impl Into<Z>) -> Self {
+        let modulus = modulus.into();
+        assert!(modulus > 1, "Modulus can not be smaller than 2.");
+
+        let mut poly = self.rem(&modulus);
+
+        for i in 0..=poly.get_degree() {
+            let mut coeff = unsafe { poly.get_coeff_unchecked(i) };
+            if coeff > modulus.div_floor(2) {
+                coeff -= &modulus;
+            }
+            unsafe { poly.set_coeff_unchecked(i, coeff) };
+        }
+
+        poly
+    }
+}
 
 #[cfg(test)]
 mod test_rem {
@@ -297,5 +339,71 @@ mod test_rem {
             % PolyOverZq::from_str("4  1 0 0 1 mod 24").unwrap();
         let _ = PolyOverZ::from_str("2  2 42").unwrap()
             % &PolyOverZq::from_str("4  1 0 0 1 mod 24").unwrap();
+    }
+}
+
+#[cfg(test)]
+mod test_mod_pm {
+    use super::Z;
+    use crate::{integer::PolyOverZ, integer_mod_q::Modulus};
+    use std::str::FromStr;
+
+    /// Testing mod± for two owned
+    #[test]
+    fn mod_pm() {
+        let a = PolyOverZ::from_str("2  2 42").unwrap();
+        let b = Z::from(25);
+        let modulus = Modulus::from(25);
+        let c1 = a.clone().mod_pm(b);
+        let c2 = a.clone().mod_pm(modulus);
+        let cmp = PolyOverZ::from_str("2  2 -8").unwrap();
+        assert_eq!(c1, cmp);
+        assert_eq!(c2, cmp);
+    }
+
+    /// Testing mod± for large numbers
+    #[test]
+    fn mod_pm_large_numbers() {
+        let a = PolyOverZ::from_str(&format!("2  2 {}", u64::MAX)).unwrap();
+        let b = Z::from(u64::MAX - 2);
+        let modulus = Modulus::from(u64::MAX - 2);
+        let c1 = a.clone().mod_pm(&b);
+        let c2 = a.clone().mod_pm(&modulus);
+        let cmp = PolyOverZ::from_str("2  2 2").unwrap();
+        assert_eq!(c1, cmp);
+        assert_eq!(c2, cmp);
+    }
+
+    /// Ensures that computing mod± a negative number results in a panic
+    #[test]
+    #[should_panic]
+    fn mod_pm_negative_error() {
+        let a = PolyOverZ::from_str("2  2 42").unwrap();
+        let b = Z::from(-24);
+        _ = &a.mod_pm(&b);
+    }
+
+    /// Ensures that computing mod± 0 results in a panic
+    #[test]
+    #[should_panic]
+    fn zero_modulus() {
+        _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(0);
+    }
+
+    /// Ensures that `mod±` is available for several types
+    #[test]
+    fn availability() {
+        let _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(2u8);
+        let _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(2u16);
+        let _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(2u32);
+        let _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(2u64);
+        let _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(2i8);
+        let _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(2i16);
+        let _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(2i32);
+        let _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(2i64);
+        let _ = PolyOverZ::from_str("2  2 42").unwrap().mod_pm(Z::from(2));
+        let _ = PolyOverZ::from_str("2  2 42")
+            .unwrap()
+            .mod_pm(Modulus::from(2));
     }
 }
